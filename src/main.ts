10813bc688813728a09ce0ae665d3eff57c6d69d
Mirone <Saul.Mirone@gmail.com>
2021-03-27 17:29:28 +0800

    add image in example

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,8 @@ const markdown = `
 > Milkdown is an editor.
 
 Hello, ***milkdown* nice \`to\` meet *you***!
+
+**Of course you can add image! ![cat](https://th.bing.com/th/id/OIP.EiYMXYhAnpsXnVmwJAq1jAHaEo?pid=ImgDet&rs=1)**
 `;
 
 new Editor(root, markdown);
