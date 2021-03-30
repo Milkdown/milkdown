f0739a3c44ccfe73165824faadbe51fa60fea8a2
Mirone <Saul.Mirone@gmail.com>
2021-03-30 22:24:51 +0800

    fix demo markdown

--- packages/core/app/main.ts
+++ packages/core/app/main.ts
@@ -2,18 +2,30 @@ import { Editor } from '../src';
 
 const markdown = `
 # Milkdown Test
+
 ## Blockquote
+
 > Milkdown is an editor.
+
 ## Marks Paragraph
+
 Hello, ***milkdown* nice \`to\` meet *you***!  
 There should be a line break before this.
+
 ---
+
 ## Image and Link
+
 **Of course you can add image! ![cat](https://th.bing.com/th/id/OIP.EiYMXYhAnpsXnVmwJAq1jAHaEo?pid=ImgDet&rs=1 "kitty")**
+
 Your *[link is here](https://bing.com "bing")*, have a look.
+
 ![Alt text][id]
+
 [id]: https://th.bing.com/th/id/OIP.EiYMXYhAnpsXnVmwJAq1jAHaEo?pid=ImgDet&rs=1  "The Cat"
+
 ## Lists
+
 * list item 1
     1. is this the real life
     2. is this just fantasy
@@ -22,12 +34,16 @@ Your *[link is here](https://bing.com "bing")*, have a look.
       som explain
     * sub list item 2
 * list item 3
+
 ## Code
+
 \`\`\`javascript
 const milkdown = new Milkdown();
 milkdown.create();
 \`\`\`
+
 ---
+
 Now you can play!
 `;
 
