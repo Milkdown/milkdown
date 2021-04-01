5c93876f88c8635824c07a421792b3b0dac3c343
Mirone Saul <Saul-Mirone@outlook.com>
2021-04-01 18:55:48 +0800

    add selection

--- packages/core/src/node/code-fence.ts
+++ packages/core/src/node/code-fence.ts
@@ -5,6 +5,21 @@ import type { SerializerNode } from '../serializer/types';
 import { textblockTypeInputRule } from 'prosemirror-inputrules';
 import { Node } from '../abstract';
 
+const languageOptions = [
+    'javascript',
+    'typescript',
+    'bash',
+    'sql',
+    'json',
+    'c',
+    'cpp',
+    'java',
+    'ruby',
+    'python',
+    'go',
+    'rust',
+];
+
 export class CodeFence extends Node {
     name = 'fence';
     schema: NodeSpec = {
@@ -22,15 +37,33 @@ export class CodeFence extends Node {
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
             { tag: 'pre', preserveWhitespace: 'full', getAttrs: (dom: any) => ({ language: dom.dataset.language }) },
         ],
-        toDOM: (node) => [
-            'div',
-            {
-                'data-language': node.attrs.language,
-                class: 'code-fence',
-                contentEditable: 'false',
-            },
-            ['pre', ['code', { spellCheck: 'false' }, 0]],
-        ],
+        toDOM: (node) => {
+            const select = document.createElement('select');
+            select.addEventListener('change', (e) => {
+                const el = e.target;
+                if (!el) return;
+                const value = (el as HTMLSelectElement).value;
+                console.log(value);
+            });
+            languageOptions.forEach((lang) => {
+                const option = document.createElement('option');
+                option.value = lang;
+                option.innerText = lang;
+                option.selected = node.attrs.language === languageOptions;
+                select.appendChild(option);
+            });
+            return [
+                'div',
+                {
+                    'data-language': node.attrs.language,
+                    class: 'code-fence',
+                    contentEditable: 'false',
+                },
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                select as any,
+                ['pre', ['code', { spellCheck: 'false' }, 0]],
+            ];
+        },
     };
     parser: ParserSpec = {
         block: this.name,
