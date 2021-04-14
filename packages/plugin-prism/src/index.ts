3026593edfc88a9ee2f0c00250bf04ac0ddaa2ba
Mirone Saul <Saul-Mirone@outlook.com>
2021-04-14 14:42:39 +0800

    feat: plugin loader

--- packages/plugin-prism/src/index.ts
+++ packages/plugin-prism/src/index.ts
@@ -1,1 +1,10 @@
-export { Prism } from './prism';
+import { createPlugin } from '@milkdown/core';
+import { Prism } from './prism';
+
+export const prismPlugin = createPlugin({
+    prosemirror: (schema) => {
+        return {
+            plugins: [Prism(schema.nodes.fence.name)],
+        };
+    },
+});
