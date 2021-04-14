c8fbbeba17fb75fe06f0767257ac0cc77fd54d16
Mirone <Saul.Mirone@gmail.com>
2021-04-15 00:16:12 +0800

    feat(core): add node and mark view

--- packages/core/src/abstract/mark.ts
+++ packages/core/src/abstract/mark.ts
@@ -1,6 +1,21 @@
-import type { MarkSpec, MarkType } from 'prosemirror-model';
+import type { MarkSpec, MarkType, Mark as ProsemirrorMark } from 'prosemirror-model';
+import type { Decoration, EditorView, NodeView } from 'prosemirror-view';
+import type { Editor } from '../editor';
 import type { SerializerMark } from '../serializer/types';
 import { Base } from './base';
+
+export type MarkViewFn = (
+    editor: Editor,
+    markType: MarkType,
+    mark: ProsemirrorMark,
+    view: EditorView,
+    getPos: boolean,
+    declarations: Decoration[],
+) => NodeView;
+
+export interface Mark {
+    readonly view?: MarkViewFn;
+}
 
 export abstract class Mark extends Base<MarkType> {
     abstract readonly schema: MarkSpec;
--- packages/core/src/abstract/node.ts
+++ packages/core/src/abstract/node.ts
@@ -1,6 +1,21 @@
-import type { NodeSpec, NodeType } from 'prosemirror-model';
+import type { NodeSpec, NodeType, Node as ProsemirrorNode } from 'prosemirror-model';
+import type { Decoration, EditorView, NodeView } from 'prosemirror-view';
+import type { Editor } from '../editor';
 import type { SerializerNode } from '../serializer/types';
 import { Base } from './base';
+
+export type NodeViewFn = (
+    editor: Editor,
+    nodeType: NodeType,
+    node: ProsemirrorNode,
+    view: EditorView,
+    getPos: () => number,
+    declarations: Decoration[],
+) => NodeView;
+
+export interface Node {
+    readonly view?: NodeViewFn;
+}
 
 export abstract class Node extends Base<NodeType> {
     abstract readonly schema: NodeSpec;
