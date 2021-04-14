7666b86940b2a8549b5bc88ca28ba76bda0bdb2c
Mirone Saul <Saul-Mirone@outlook.com>
2021-04-14 14:48:27 +0800

    chore: add error message

--- packages/plugin-prism/src/index.ts
+++ packages/plugin-prism/src/index.ts
@@ -3,6 +3,9 @@ import { Prism } from './prism';
 
 export const prismPlugin = createPlugin({
     prosemirror: (schema) => {
+        if (!schema.nodes.fence) {
+            throw new Error('[Milkdown Prism Plugin]: load failed, No code fence in schema!');
+        }
         return {
             plugins: [Prism(schema.nodes.fence.name)],
         };
