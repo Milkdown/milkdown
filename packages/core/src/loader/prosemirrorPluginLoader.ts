45d0df4722c24704b6580cb46f67d8fc808eaef1
Mirone <Saul.Mirone@gmail.com>
2021-04-20 00:32:00 +0800

    feat: add ctx of prosemirror plugin loader

--- packages/core/src/loader/prosemirrorPluginLoader.ts
+++ packages/core/src/loader/prosemirrorPluginLoader.ts
@@ -4,19 +4,23 @@ import { AtomType, LoadState } from '../constant';
 import { PluginReadyContext } from '../editor';
 
 const prosemirrorPluginLoader = (id: string) => {
-    return class ProsemirrorPluginLoader extends Atom<PluginReadyContext, PluginReadyContext, { plugins: Plugin[] }> {
+    return class ProsemirrorPluginLoader extends Atom<
+        PluginReadyContext,
+        PluginReadyContext,
+        { plugins: (ctx: PluginReadyContext) => Plugin[] }
+    > {
         id = id;
         type = AtomType.ProsemirrorPlugin;
         loadAfter = LoadState.SchemaReady;
         main() {
             this.updateContext({
-                prosemirrorPlugins: this.context.prosemirrorPlugins.concat(...this.options.plugins),
+                prosemirrorPlugins: this.context.prosemirrorPlugins.concat(...this.options.plugins(this.context)),
             });
         }
     };
 };
 
-export const createProsemirrorPlugin = (id: string, plugins: Plugin[]) => {
+export const createProsemirrorPlugin = (id: string, plugins: (ctx: PluginReadyContext) => Plugin[]) => {
     const Factory = prosemirrorPluginLoader(id);
     return new Factory({ plugins });
 };
