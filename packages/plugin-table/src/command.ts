4a230c2b8da2b83ddbdd0f2ef36ec2fe1dc0b061
Mirone <Saul.Mirone@gmail.com>
2021-05-09 20:49:42 +0800

    feat: add exit table command

---
+++ packages/plugin-table/src/command.ts
@@ -0,0 +1,16 @@
+import { Selection } from 'prosemirror-state';
+import { Node, NodeType } from 'prosemirror-model';
+import { Command } from 'prosemirror-commands';
+import { isInTable } from 'prosemirror-tables';
+
+export const exitTable = (node: NodeType): Command => (state, dispatch) => {
+    if (!isInTable(state)) {
+        return false;
+    }
+    const { $head } = state.selection;
+    const pos = $head.after();
+    const tr = state.tr.replaceWith(pos, pos, node.createAndFill() as Node);
+    tr.setSelection(Selection.near(tr.doc.resolve(pos), 1));
+    dispatch?.(tr.scrollIntoView());
+    return true;
+};
--- packages/plugin-table/src/index.ts
+++ packages/plugin-table/src/index.ts
@@ -4,14 +4,16 @@ import { keymap } from 'prosemirror-keymap';
 import { nodes } from './nodes';
 import { tableOperatorPlugin } from './table-operator-plugin';
 import { markdownItTablePlugin } from './markdown-it-table-plugin';
+import { exitTable } from './command';
 
-const plugin = createProsemirrorPlugin('milkdown-table', () => [
+const plugin = createProsemirrorPlugin('milkdown-table', (ctx) => [
     columnResizing({}),
     tableEditing(),
     tableOperatorPlugin(),
     keymap({
         'Mod-]': goToNextCell(1),
         'Mod-[': goToNextCell(-1),
+        'Mod-Enter': exitTable(ctx.schema.nodes.paragraph),
     }),
 ]);
 
