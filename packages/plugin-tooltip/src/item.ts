a75a35538d714c65843c2ac0e0a8b2f4603ea55e
Mirone Saul <Saul-Mirone@outlook.com>
2021-05-21 18:41:27 +0800

    feat: tooltip style optimize

--- packages/plugin-tooltip/src/item.ts
+++ packages/plugin-tooltip/src/item.ts
@@ -4,13 +4,13 @@ import { MarkType } from 'prosemirror-model';
 import { EditorView } from 'prosemirror-view';
 import {
     icon,
-    input,
+    // input,
     hasMark,
     isTextAndNotHasMark,
-    findMarkByType,
-    modifyLink,
-    findChildNode,
-    modifyImage,
+    // findMarkByType,
+    // modifyLink,
+    // findChildNode,
+    // modifyImage,
 } from './utility';
 
 export type Pred = (view: EditorView) => boolean;
@@ -30,9 +30,9 @@ export enum Action {
     ToggleItalic,
     ToggleCode,
     ToggleLink,
-    ModifyLink,
-    ModifyImageSrc,
-    ModifyImageAlt,
+    // ModifyLink,
+    // ModifyImageSrc,
+    // ModifyImageAlt,
 }
 
 export type ItemMap = Record<Action, Item>;
@@ -50,61 +50,62 @@ const createToggleIcon = (
 });
 
 export const itemMap = (ctx: PluginReadyContext): ItemMap => {
-    const { marks, nodes } = ctx.schema;
+    // const { marks, nodes } = ctx.schema;
+    const { marks } = ctx.schema;
     return {
         [Action.ToggleBold]: createToggleIcon('format_bold', marks.strong, marks.code_inline),
         [Action.ToggleItalic]: createToggleIcon('format_italic', marks.em, marks.code_inline),
         [Action.ToggleCode]: createToggleIcon('code', marks.code_inline, marks.link),
         [Action.ToggleLink]: createToggleIcon('link', marks.link, marks.code_inline, { href: '' }),
-        [Action.ModifyLink]: {
-            $: input(),
-            command: modifyLink(ctx.schema),
-            active: () => false,
-            disable: (view) => !hasMark(view.state, marks.link),
-            update: (view, $) => {
-                const { firstChild } = $;
-                if (!(firstChild instanceof HTMLInputElement)) return;
+        // [Action.ModifyLink]: {
+        //     $: input(),
+        //     command: modifyLink(ctx.schema),
+        //     active: () => false,
+        //     disable: (view) => !hasMark(view.state, marks.link),
+        //     update: (view, $) => {
+        //         const { firstChild } = $;
+        //         if (!(firstChild instanceof HTMLInputElement)) return;
 
-                const node = findMarkByType(view.state, marks.link);
-                if (!node) return;
+        //         const node = findMarkByType(view.state, marks.link);
+        //         if (!node) return;
 
-                const mark = node.marks.find((m) => m.type === marks.link);
-                if (!mark) return;
+        //         const mark = node.marks.find((m) => m.type === marks.link);
+        //         if (!mark) return;
 
-                firstChild.value = mark.attrs.href;
-            },
-        },
-        [Action.ModifyImageSrc]: {
-            $: input('link'),
-            command: modifyImage(ctx.schema, 'src'),
-            active: () => false,
-            disable: (view) => !findChildNode(view.state.selection, nodes.image),
-            update: (view, $) => {
-                // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                const [_, input] = Array.from($.children);
-                if (!(input instanceof HTMLInputElement)) return;
+        //         firstChild.value = mark.attrs.href;
+        //     },
+        // },
+        // [Action.ModifyImageSrc]: {
+        //     $: input('link'),
+        //     command: modifyImage(ctx.schema, 'src'),
+        //     active: () => false,
+        //     disable: (view) => !findChildNode(view.state.selection, nodes.image),
+        //     update: (view, $) => {
+        //         // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        //         const [_, input] = Array.from($.children);
+        //         if (!(input instanceof HTMLInputElement)) return;
 
-                const node = findChildNode(view.state.selection, nodes.image);
-                if (!node) return;
+        //         const node = findChildNode(view.state.selection, nodes.image);
+        //         if (!node) return;
 
-                input.value = node.node.attrs.src;
-            },
-        },
-        [Action.ModifyImageAlt]: {
-            $: input('alt'),
-            command: modifyImage(ctx.schema, 'alt'),
-            active: () => false,
-            disable: (view) => !findChildNode(view.state.selection, nodes.image),
-            update: (view, $) => {
-                // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                const [_, input] = Array.from($.children);
-                if (!(input instanceof HTMLInputElement)) return;
+        //         input.value = node.node.attrs.src;
+        //     },
+        // },
+        // [Action.ModifyImageAlt]: {
+        //     $: input('alt'),
+        //     command: modifyImage(ctx.schema, 'alt'),
+        //     active: () => false,
+        //     disable: (view) => !findChildNode(view.state.selection, nodes.image),
+        //     update: (view, $) => {
+        //         // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        //         const [_, input] = Array.from($.children);
+        //         if (!(input instanceof HTMLInputElement)) return;
 
-                const node = findChildNode(view.state.selection, nodes.image);
-                if (!node) return;
+        //         const node = findChildNode(view.state.selection, nodes.image);
+        //         if (!node) return;
 
-                input.value = node.node.attrs.alt;
-            },
-        },
+        //         input.value = node.node.attrs.alt;
+        //     },
+        // },
     };
 };
