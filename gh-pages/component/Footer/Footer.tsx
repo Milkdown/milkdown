69dd5c5291fa6742244eac9b40b4dd62ddfa679c
Mirone <Saul.Mirone@gmail.com>
2021-05-29 21:13:48 +0800

    chore: optimize theme

--- gh-pages/component/Footer/Footer.tsx
+++ gh-pages/component/Footer/Footer.tsx
@@ -1,4 +1,3 @@
-import { LocationType, useLocationType } from '../hooks/useLocationType';
 import React from 'react';
 
 import className from './style.module.css';
@@ -6,9 +5,6 @@ import className from './style.module.css';
 const copyright = 'MIT Licensed | Copyright © 2021-present Mirone Saul';
 
 export const Footer: React.FC = () => {
-    const locationType = useLocationType();
 
-    const classes = locationType === LocationType.Home ? [className.footer, className.footerHome] : [className.footer];
-
-    return <footer className={classes.join(' ')}>{copyright}</footer>;
+    return <footer className={className.footer}>{copyright}</footer>;
 };
