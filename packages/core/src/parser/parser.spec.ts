7b6db46249fb28174a824d68b4598375acf537ad
Saul Mirone <Saul-Mirone@outlook.com>
2021-06-27 18:16:35 +0000

    feat: switch to remark

--- packages/core/src/parser/parser.spec.ts
+++ packages/core/src/parser/parser.spec.ts
@@ -1,12 +1,14 @@
+import remark from 'remark';
 import { parse } from './parser';
 import { Node } from 'unist';
 
 const markdown = `*my* paragraph`;
 
-test('parser', () => {
+test('Parser/Test', () => {
     const node: Node = parse(markdown);
 
-    console.log((node.children as Node[])[0]);
+    console.log(remark.stringify(node));
+    // console.log((node.children as Node[])[0]);
     // console.log((node.children as any)[0].children[0]);
     // console.log(JSON.stringify(node, null, 4));
 });
