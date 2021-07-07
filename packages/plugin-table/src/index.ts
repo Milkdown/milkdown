f7d3dd8205dd78f5d86fddc1f280c81a041f85f6
Saul Mirone <Saul-Mirone@outlook.com>
2021-07-07 00:24:19 +0000

    refactor: optimize table plugin

--- packages/plugin-table/src/index.ts
+++ packages/plugin-table/src/index.ts
@@ -1,52 +1,10 @@
-import { createProsemirrorPlugin, createRemarkPlugin, LoadPluginContext } from '@milkdown/core';
-import { columnResizing, goToNextCell, tableEditing } from 'prosemirror-tables';
+import { createRemarkPlugin } from '@milkdown/core';
 import gfm from 'remark-gfm';
-import { keymap } from 'prosemirror-keymap';
-import { Keymap, Command } from 'prosemirror-commands';
+import { keymapPlugin, KeymapPluginOptions } from './keymap';
 import { nodes } from './nodes';
-import { tableOperatorPlugin } from './table-operator-plugin';
-import { exitTable } from './command';
-
-export enum SupportedKeys {
-    NextCell = 'NextCell',
-    PrevCell = 'PrevCell',
-    ExitTable = 'ExitTable',
-}
-export type KeyMap = Partial<Record<SupportedKeys, string>>;
-
-const defaultKeymap: Required<KeyMap> = {
-    [SupportedKeys.NextCell]: 'Mod-]',
-    [SupportedKeys.PrevCell]: 'Mod-[',
-    [SupportedKeys.ExitTable]: 'Mod-Enter',
-};
-
-const keymapCommands = (ctx: LoadPluginContext): Record<SupportedKeys, Command> => ({
-    [SupportedKeys.NextCell]: goToNextCell(1),
-    [SupportedKeys.PrevCell]: goToNextCell(-1),
-    [SupportedKeys.ExitTable]: exitTable(ctx.schema.nodes.paragraph),
-});
-
-type PluginOptions = { keymap?: KeyMap };
-const plugin = (options: PluginOptions = {}) =>
-    createProsemirrorPlugin('milkdown-table', (ctx) => {
-        const customKeyMap = {
-            ...defaultKeymap,
-            ...(options.keymap || {}),
-        };
-        const commands = keymapCommands(ctx);
-        const keymapValue: Keymap = Object.entries(customKeyMap)
-            .filter((x): x is [SupportedKeys, string] => !!x)
-            .reduce((acc, [name, key]) => {
-                return {
-                    ...acc,
-                    [key]: commands[name],
-                };
-            }, {} as Record<string, Command>);
-        return [columnResizing({}), tableEditing(), tableOperatorPlugin(), keymap(keymapValue)];
-    });
 
 const remarkGFMPlugin = createRemarkPlugin('remark-table-markdown', () => [gfm]);
 
-export const table = (options: PluginOptions = {}) => [...nodes, remarkGFMPlugin, plugin(options)];
+export const table = (options: KeymapPluginOptions = {}) => [...nodes, remarkGFMPlugin, keymapPlugin(options)];
 
 export { createTable } from './utils';
