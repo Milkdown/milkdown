27d89a9a3c6799dc4f230f3df217df0d97a28ed1
Saul Mirone <Saul-Mirone@outlook.com>
2021-07-10 20:18:50 +0000

    fix: doc style issue

--- gh-pages/component/Demo/Demo.tsx
+++ gh-pages/component/Demo/Demo.tsx
@@ -119,7 +119,7 @@ export const Demo = ({ content, mode, isDarkMode }: DemoProps): JSX.Element => {
 
     return (
         <div className={classes}>
-            <div className={className.milk}>
+            <div className="milk">
                 <MilkdownEditor
                     key={'milkdown' + stateCountMilkdown.toString()}
                     content={defaultValueForMilkdown.current}
