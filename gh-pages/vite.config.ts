9f0dcd7345c10f5dcffea3b088684c5ffa0876a3
Saul Mirone <Saul-Mirone@outlook.com>
2021-07-22 01:40:40 +0000

    fix: deps

--- gh-pages/vite.config.ts
+++ gh-pages/vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
             'prosemirror-history',
             'prosemirror-transform',
             'prosemirror-gapcursor',
+            'prosemirror-dropcursor',
         ],
     },
     plugins: [markdownPlugin(), reactRefresh()],
