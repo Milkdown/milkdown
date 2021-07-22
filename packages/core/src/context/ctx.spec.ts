427817fdb65f69d807ccc00f7a4308597f97466f
Saul Mirone <Saul-Mirone@outlook.com>
2021-07-22 07:54:23 +0000

    test: add test for ctx

--- packages/core/src/context/ctx.spec.ts
+++ packages/core/src/context/ctx.spec.ts
@@ -1,9 +1,35 @@
 import { createCtx } from './ctx';
 
-test('createCtx', () => {
+test('primitive ctx', () => {
     const factory = createCtx(0);
     const map = new Map();
     const ctx = factory(map);
 
     expect(ctx.get()).toBe(0);
+
+    ctx.set(20);
+    expect(ctx.get()).toBe(20);
+
+    ctx.update((x) => x + 1);
+    expect(ctx.get()).toBe(21);
+});
+
+test('structure ctx', () => {
+    const factory = createCtx<number[]>([]);
+    const map1 = new Map();
+    const ctx1 = factory(map1);
+
+    const map2 = new Map();
+    const ctx2 = factory(map2);
+
+    expect(ctx1.get()).toEqual([]);
+    ctx1.set([1]);
+    expect(ctx1.get()).toEqual([1]);
+
+    expect(ctx2.get()).toEqual([]);
+
+    ctx1.update((x) => x.concat(3));
+    expect(ctx1.get()).toEqual([1, 3]);
+
+    expect(ctx2.get()).toEqual([]);
 });
