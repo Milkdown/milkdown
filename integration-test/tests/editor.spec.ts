02e3421eb47be1710adfa98b58f94cbbc5539a91
Saul-Mirone <Saul-Mirone@outlook.com>
2021-07-25 03:33:54 +0800

    fix: pw it

--- integration-test/tests/editor.spec.ts
+++ integration-test/tests/editor.spec.ts
@@ -13,6 +13,5 @@ test('has editor', async ({ page }) => {
     const milkdown = await page.waitForSelector('.milkdown');
     const editor = await milkdown.waitForSelector('.editor');
     expect(await editor.getAttribute('contenteditable')).toBe('true');
-
-    expect(await page.screenshot({ fullPage: true })).toMatchSnapshot('editor-overview.png');
+    expect(await editor.waitForSelector('.h1 >> text=Milkdown')).toBeTruthy();
 });
