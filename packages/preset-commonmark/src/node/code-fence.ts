2d84bce73a8198990abcb4056abf4c895c104453
Mirone <Saul-Mirone@outlook.com>
2021-08-13 13:08:42 +0000

    feat: 🎸 optimize code fence button style

--- packages/preset-commonmark/src/node/code-fence.ts
+++ packages/preset-commonmark/src/node/code-fence.ts
@@ -43,6 +43,9 @@ export const codeFence = createNode<Keys, { languageList?: string[] }>((options,
             language: {
                 default: '',
             },
+            fold: {
+                default: true,
+            },
         },
         parseDOM: [
             {
@@ -99,47 +102,84 @@ export const codeFence = createNode<Keys, { languageList?: string[] }>((options,
         }
         const container = document.createElement('div');
         const selectWrapper = document.createElement('div');
-        const select = document.createElement('select');
+        const select = document.createElement('ul');
         const pre = document.createElement('pre');
         const code = document.createElement('code');
 
+        const valueWrapper = document.createElement('div');
+        valueWrapper.className = 'code-fence_value';
+        const value = document.createElement('span');
+        const button = document.createElement('span');
+        button.className = 'icon material-icons material-icons-outlined';
+        button.textContent = 'expand_more';
+        valueWrapper.appendChild(value);
+        valueWrapper.appendChild(button);
+
         select.className = 'code-fence_select';
         select.addEventListener('mousedown', (e) => {
-            if (view.editable) return;
-
             e.preventDefault();
+            e.stopPropagation();
+
+            if (!view.editable) return;
+
+            const el = e.target;
+            if (!(el instanceof HTMLLIElement)) return;
+            const { tr } = view.state;
+
+            view.dispatch(
+                tr.setNodeMarkup(getPos(), undefined, {
+                    fold: true,
+                    language: el.dataset.value,
+                }),
+            );
         });
-        select.addEventListener('change', (e) => {
-            const el = e.target as HTMLSelectElement | null;
-            if (!el) return;
+        valueWrapper.addEventListener('mousedown', (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+
+            if (!view.editable) return;
+            const { tr } = view.state;
+
+            view.dispatch(
+                tr.setNodeMarkup(getPos(), undefined, {
+                    fold: false,
+                    language: container.dataset.language,
+                }),
+            );
+        });
+        document.addEventListener('mousedown', () => {
+            if (!view.editable || select.dataset.fold === 'true') return;
 
             const { tr } = view.state;
 
             view.dispatch(
                 tr.setNodeMarkup(getPos(), undefined, {
-                    language: el.value,
+                    fold: true,
+                    language: container.dataset.language,
                 }),
             );
         });
 
         languageOptions.concat(options?.languageList || []).forEach((lang) => {
-            const option = document.createElement('option');
+            const option = document.createElement('li');
             option.className = 'code-fence_select-option';
-            option.value = lang;
             option.innerText = lang || '--';
-            option.selected = node.attrs.language === lang;
             select.appendChild(option);
+            option.setAttribute('data-value', lang);
         });
 
         code.spellcheck = false;
+        selectWrapper.className = 'code-fence_select-wrapper';
         selectWrapper.contentEditable = 'false';
-
+        selectWrapper.append(valueWrapper);
         selectWrapper.append(select);
         pre.append(code);
-        container.append(selectWrapper, pre);
 
+        container.append(selectWrapper, pre);
         container.setAttribute('class', utils.getClassName(node.attrs, 'code-fence'));
         container.setAttribute('data-language', node.attrs.language);
+        value.innerText = node.attrs.language || '--';
+        select.setAttribute('data-fold', node.attrs.fold ? 'true' : 'false');
 
         return {
             dom: container,
@@ -147,7 +187,10 @@ export const codeFence = createNode<Keys, { languageList?: string[] }>((options,
             update: (updatedNode) => {
                 if (updatedNode.type.name !== id) return false;
 
-                container.dataset.language = updatedNode.attrs.language;
+                const lang = updatedNode.attrs.language;
+                container.dataset.language = lang;
+                value.innerText = lang || '--';
+                select.setAttribute('data-fold', updatedNode.attrs.fold ? 'true' : 'false');
 
                 return true;
             },
