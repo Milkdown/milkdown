c1d0a24db9f792ab73a197f3b73de9bbdeeacd02
Mirone <Saul-Mirone@outlook.com>
2021-08-16 05:43:55 +0000

    feat: 🎸 add more renderer type

--- packages/design-system/src/mixins/renderer.ts
+++ packages/design-system/src/mixins/renderer.ts
@@ -3,9 +3,12 @@ import { AtRule } from 'postcss';
 type ResultBasic = string | null | undefined | { [name: string]: ResultBasic };
 type Result = Record<string, ResultBasic>;
 
-type Mixin<T extends unknown[] = []> = (mixin: AtRule, ...params: T[]) => void | Result;
+export type Mixin<T extends unknown[] = []> = (mixin: AtRule, ...params: T[]) => void | Result;
 
 export interface Renderer {
-    Icon: Mixin<[string]>;
-    ScrollBar: Mixin<['col' | 'row']>;
+    Icon: Mixin<[type: string]>;
+    ScrollBar: Mixin<[direction: 'col' | 'row']>;
+    Shadow: Mixin;
+    Line: Mixin;
+    Border: Mixin;
 }
