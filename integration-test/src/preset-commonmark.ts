d94254e22ed01cb21db994c9226cbc7118bb4427
Saul-Mirone <Saul-Mirone@outlook.com>
2021-08-18 03:29:01 +0800

    fix: 🐛 integration test

--- integration-test/src/preset-commonmark.ts
+++ integration-test/src/preset-commonmark.ts
@@ -1,14 +1,13 @@
 import { Editor, rootCtx } from '@milkdown/core';
 import { commonmark } from '@milkdown/preset-commonmark';
-
-import '@milkdown/theme-nord/lib/theme.css';
-import '@milkdown/preset-commonmark/lib/style.css';
+import { nord } from '@milkdown/theme-nord';
 
 export const setup = () => {
     return new Editor()
         .config((ctx) => {
             ctx.set(rootCtx, document.getElementById('app'));
         })
+        .use(nord)
         .use(commonmark)
         .create();
 };
