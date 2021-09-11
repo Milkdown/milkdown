8724b5aad61dcd93a79bde98fec0131e9bdb0cb6
Mirone <Saul.Mirone@gmail.com>
2021-09-11 16:34:56 +0800

    feat: 🎸 add id of slash actions

--- packages/plugin-slash/src/item.ts
+++ packages/plugin-slash/src/item.ts
@@ -5,19 +5,21 @@ import type { Schema } from 'prosemirror-model';
 import { cleanUpAndCreateNode } from './utility';
 
 export type Action = {
+    id: string;
     $: HTMLElement;
     keyword: string[];
     command: Command;
     enable: (schema: Schema) => boolean;
 };
 
-export type WrappedAction = Pick<Action, 'keyword'> & {
+export type WrappedAction = Pick<Action, 'keyword' | 'id'> & {
     enable: (schema: Schema) => boolean;
     command: () => void;
     dom: HTMLElement;
 };
 
 export const transformAction = (action: WrappedAction): Action => ({
+    id: action.id,
     keyword: action.keyword,
     $: action.dom,
     command: cleanUpAndCreateNode(action.command),
