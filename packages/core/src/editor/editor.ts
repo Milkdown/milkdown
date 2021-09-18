bcba794b494473dbb25029b143e0904592d03145
Mirone <Saul.Mirone@gmail.com>
2021-09-19 00:17:35 +0800

    refactor: 💡 add tsdoc for editor module

--- packages/core/src/editor/editor.ts
+++ packages/core/src/editor/editor.ts
@@ -35,16 +35,10 @@ export class Editor {
     #clock = createClock();
 
     #plugins: Set<CtxHandler> = new Set();
+    #configureList: Configure[] = [];
 
     #ctx = new Ctx(this.#container, this.#clock);
     #pre = new Pre(this.#container, this.#clock);
-
-    use = (plugins: MilkdownPlugin | MilkdownPlugin[]) => {
-        [plugins].flat().forEach((plugin) => {
-            this.#plugins.add(plugin(this.#pre));
-        });
-        return this;
-    };
 
     #loadInternal = () => {
         const internalPlugins = [
@@ -64,6 +58,47 @@ export class Editor {
         this.use(internalPlugins.concat(init(this)).concat(configPlugin));
     };
 
+    /**
+     * Use one plugin or a list of plugins for current editor.
+     *
+     * @example
+     * ```
+     * Editor.make()
+     *   .use(plugin)
+     *   .use([pluginA, pluginB])
+     * ```
+     *
+     * @param plugins - A list of plugins, or one plugin.
+     * @returns Editor instance.
+     */
+    use = (plugins: MilkdownPlugin | MilkdownPlugin[]) => {
+        [plugins].flat().forEach((plugin) => {
+            this.#plugins.add(plugin(this.#pre));
+        });
+        return this;
+    };
+
+    /**
+     * Config the context for current editor.
+     *
+     * @param configure - The function that configure current editor, can be async, with context as parameter.
+     * @returns Editor instance.
+     */
+    config = (configure: Configure) => {
+        this.#configureList.push(configure);
+        return this;
+    };
+
+    /**
+     * Create the editor UI.
+     *
+     * @example
+     * ```
+     * Editor.make().use(nord).use(commonmark).create()
+     * ```
+     *
+     * @returns A promise object, will be resolved as editor instance after create finish.
+     */
     create = async () => {
         this.#loadInternal();
         await Promise.all(
@@ -74,12 +109,29 @@ export class Editor {
         return this;
     };
 
-    #configureList: Configure[] = [];
-
-    config = (configure: Configure) => {
-        this.#configureList.push(configure);
-        return this;
-    };
-
+    /**
+     * Get the context value in a running editor on demand and return the action result.
+     *
+     * @example
+     * ```
+     * import { Editor, editorViewCtx, serializerCtx } from '@milkdown/core';
+     * async function playWithEditor() {
+     *     const editor = await Editor.make().use(commonmark).create();
+     *
+     *     const getMarkdown = () =>
+     *         editor.action((ctx) => {
+     *             const editorView = ctx.get(editorViewCtx);
+     *             const serializer = ctx.get(serializerCtx);
+     *             return serializer(editorView.state.doc);
+     *         });
+     *
+     *     // get markdown string:
+     *     getMarkdown();
+     * }
+     * ```
+     *
+     * @param action - The function that get editor context and return the action result.
+     * @returns The action result.
+     */
     action = <T>(action: (ctx: Ctx) => T) => action(this.#ctx);
 }
