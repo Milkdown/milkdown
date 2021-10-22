27cda2d801dcef0a33c5c18da2cc6e80f1ed954c
Mirone <Saul-Mirone@outlook.com>
2021-10-22 12:21:00 +0000

    feat: 🎸 optimize tooltip position calculate

--- packages/plugin-tooltip/src/button-manager/calc-button-pos.ts
+++ packages/plugin-tooltip/src/button-manager/calc-button-pos.ts
@@ -6,9 +6,13 @@ export const calcButtonPos = (buttons: HTMLElement, view: EditorView) => {
     calculateTextPosition(view, buttons, (start, end, target, parent) => {
         const selectionWidth = end.left - start.left;
         let left = start.left - parent.left - (target.width - selectionWidth) / 2;
-        const top = start.top - parent.top - target.height - 14;
+        let top = start.top - parent.top - target.height - 14;
 
         if (left < 0) left = 0;
+
+        if (start.top < target.height) {
+            top = start.bottom - parent.top + 14;
+        }
 
         return [top, left];
     });
