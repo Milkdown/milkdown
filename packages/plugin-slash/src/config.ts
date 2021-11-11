2b4edb3f2d9ef8a01ed61eacef9f697565808d0a
Mirone <Saul-Mirone@outlook.com>
2021-11-11 12:59:17 +0000

    feat: 🎸 remove keyword in slash actions

--- packages/plugin-slash/src/config.ts
+++ packages/plugin-slash/src/config.ts
@@ -33,113 +33,96 @@ export type StatusConfigBuilder = (params: StatusConfigBuilderParams) => StatusC
 
 export type Config = (ctx: Ctx) => StatusConfigBuilder;
 
-export const defaultActions = (ctx: Ctx): WrappedAction[] => {
-    const nodes = ctx.get(schemaCtx).nodes;
-    const actions: WrappedAction[] = [];
-
-    if (nodes['heading']) {
-        actions.push(
-            ...[
-                {
-                    id: 'h1',
-                    dom: createDropdownItem(ctx.get(themeToolCtx), 'Large Heading', 'h1'),
-                    command: () => ctx.get(commandsCtx).call(TurnIntoHeading, 1),
-                    keyword: ['h1', 'large heading'],
-                },
-                {
-                    id: 'h2',
-                    dom: createDropdownItem(ctx.get(themeToolCtx), 'Medium Heading', 'h2'),
-                    command: () => ctx.get(commandsCtx).call(TurnIntoHeading, 2),
-                    keyword: ['h2', 'medium heading'],
-                },
-                {
-                    id: 'h3',
-                    dom: createDropdownItem(ctx.get(themeToolCtx), 'Small Heading', 'h3'),
-                    command: () => ctx.get(commandsCtx).call(TurnIntoHeading, 3),
-                    keyword: ['h3', 'small heading'],
-                },
-            ],
-        );
-    }
-
-    if (nodes['bullet_list']) {
-        actions.push({
+export const defaultActions = (ctx: Ctx, input = '/'): WrappedAction[] => {
+    const { nodes } = ctx.get(schemaCtx);
+    const actions: Array<WrappedAction & { keyword: string[]; typeName: string }> = [
+        {
+            id: 'h1',
+            dom: createDropdownItem(ctx.get(themeToolCtx), 'Large Heading', 'h1'),
+            command: () => ctx.get(commandsCtx).call(TurnIntoHeading, 1),
+            keyword: ['h1', 'large heading'],
+            typeName: 'heading',
+        },
+        {
+            id: 'h2',
+            dom: createDropdownItem(ctx.get(themeToolCtx), 'Medium Heading', 'h2'),
+            command: () => ctx.get(commandsCtx).call(TurnIntoHeading, 2),
+            keyword: ['h2', 'medium heading'],
+            typeName: 'heading',
+        },
+        {
+            id: 'h3',
+            dom: createDropdownItem(ctx.get(themeToolCtx), 'Small Heading', 'h3'),
+            command: () => ctx.get(commandsCtx).call(TurnIntoHeading, 3),
+            keyword: ['h3', 'small heading'],
+            typeName: 'heading',
+        },
+        {
             id: 'bulletList',
             dom: createDropdownItem(ctx.get(themeToolCtx), 'Bullet List', 'bulletList'),
             command: () => ctx.get(commandsCtx).call(WrapInBulletList),
             keyword: ['bullet list', 'ul'],
-        });
-    }
-
-    if (nodes['ordered_list']) {
-        actions.push({
+            typeName: 'bullet_list',
+        },
+        {
             id: 'orderedList',
             dom: createDropdownItem(ctx.get(themeToolCtx), 'Ordered List', 'orderedList'),
             command: () => ctx.get(commandsCtx).call(WrapInOrderedList),
             keyword: ['ordered list', 'ol'],
-        });
-    }
-
-    if (nodes['task_list_item']) {
-        actions.push({
+            typeName: 'ordered_list',
+        },
+        {
             id: 'taskList',
             dom: createDropdownItem(ctx.get(themeToolCtx), 'Task List', 'taskList'),
             command: () => ctx.get(commandsCtx).call(TurnIntoTaskList),
             keyword: ['task list', 'task'],
-        });
-    }
-
-    if (nodes['image']) {
-        actions.push({
+            typeName: 'task_list_item',
+        },
+        {
             id: 'image',
             dom: createDropdownItem(ctx.get(themeToolCtx), 'Image', 'image'),
             command: () => ctx.get(commandsCtx).call(InsertImage),
             keyword: ['image'],
-        });
-    }
-
-    if (nodes['blockquote']) {
-        actions.push({
+            typeName: 'image',
+        },
+        {
             id: 'blockquote',
             dom: createDropdownItem(ctx.get(themeToolCtx), 'Quote', 'quote'),
             command: () => ctx.get(commandsCtx).call(WrapInBlockquote),
             keyword: ['quote', 'blockquote'],
-        });
-    }
-
-    if (nodes['table']) {
-        actions.push({
+            typeName: 'blockquote',
+        },
+        {
             id: 'table',
             dom: createDropdownItem(ctx.get(themeToolCtx), 'Table', 'table'),
             command: () => ctx.get(commandsCtx).call(InsertTable),
             keyword: ['table'],
-        });
-    }
-
-    if (nodes['fence']) {
-        actions.push({
+            typeName: 'table',
+        },
+        {
             id: 'code',
             dom: createDropdownItem(ctx.get(themeToolCtx), 'Code Fence', 'code'),
             command: () => ctx.get(commandsCtx).call(TurnIntoCodeFence),
             keyword: ['code'],
-        });
-    }
-
-    if (nodes['hr']) {
-        actions.push({
+            typeName: 'fence',
+        },
+        {
             id: 'divider',
             dom: createDropdownItem(ctx.get(themeToolCtx), 'Divide Line', 'divider'),
             command: () => ctx.get(commandsCtx).call(InsertHr),
             keyword: ['divider', 'hr'],
-        });
-    }
+            typeName: 'hr',
+        },
+    ];
 
-    return actions;
+    const userInput = input.slice(1).toLocaleLowerCase();
+
+    return actions
+        .filter((action) => !!nodes[action.typeName] && action.keyword.some((keyword) => keyword.includes(userInput)))
+        .map(({ keyword, typeName, ...action }) => action);
 };
 
 export const defaultConfig: Config = (ctx) => {
-    const actions = defaultActions(ctx);
-
     return ({ content, isTopLevel }) => {
         if (!isTopLevel) return null;
 
@@ -151,12 +134,10 @@ export const defaultConfig: Config = (ctx) => {
             return content === '/'
                 ? {
                       placeholder: 'Type to filter...',
-                      actions,
+                      actions: defaultActions(ctx),
                   }
                 : {
-                      actions: actions.filter(({ keyword }) =>
-                          keyword.some((key) => key.includes(content.slice(1).toLocaleLowerCase())),
-                      ),
+                      actions: defaultActions(ctx, content),
                   };
         }
 
