99d2053768984c26ff6daaa6dee0d07990d6fdbc
Mirone <Saul-Mirone@outlook.com>
2021-11-26 03:20:50 +0000

    fix: 🐛 editor area issue

--- packages/plugin-menu/src/index.ts
+++ packages/plugin-menu/src/index.ts
@@ -9,6 +9,23 @@ export const menu = createPlugin(() => {
         prosePlugins: () => {
             const plugin = new Plugin({
                 key: menuKey,
+                view: (editorView) => {
+                    const menuWrapper = document.createElement('div');
+                    const menu = document.createElement('div');
+                    menuWrapper.appendChild(menu);
+                    menuWrapper.classList.add('milkdown-menu-wrapper');
+                    menu.classList.add('milkdown-menu');
+
+                    const editorDom = editorView.dom;
+                    const parent = editorDom.parentNode;
+                    if (!parent) {
+                        throw new Error('No parent node found');
+                    }
+                    parent.replaceChild(menuWrapper, editorDom);
+                    menuWrapper.appendChild(editorDom);
+
+                    return {};
+                },
             });
 
             return [plugin];
