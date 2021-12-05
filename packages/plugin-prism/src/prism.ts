d48a2d91147eaa247a2611c04a1ea5aec0210c3c
SaulMirone <mirone@192.168.0.108>
2021-12-05 13:46:18 +0800

    fix: 🐛 codeblock highlight disappear after drag and drop

--- packages/plugin-prism/src/prism.ts
+++ packages/plugin-prism/src/prism.ts
@@ -22,7 +22,17 @@ export function Prism(name: string) {
                     (isNodeName ||
                         isPreviousNodeName ||
                         oldNode.length !== newNode.length ||
-                        oldNode[0]?.node.attrs.language !== newNode[0]?.node.attrs.language);
+                        oldNode[0]?.node.attrs.language !== newNode[0]?.node.attrs.language ||
+                        transaction.steps.some((step) => {
+                            const s = step as unknown as { from: number; to: number };
+                            return (
+                                s.from !== undefined &&
+                                s.to !== undefined &&
+                                oldNode.some((node) => {
+                                    return node.pos >= s.from && node.pos + node.node.nodeSize <= s.to;
+                                })
+                            );
+                        }));
 
                 if (codeBlockChanged) {
                     return getDecorations(transaction.doc, name);
