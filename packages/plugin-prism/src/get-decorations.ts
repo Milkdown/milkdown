ab6af7be0d98cada8657931f40d976bb85d1ae8b
Mirone <Saul-Mirone@outlook.com>
2021-12-12 07:14:44 +0000

    fix: 🐛 unsupported language of prism cause error

--- packages/plugin-prism/src/get-decorations.ts
+++ packages/plugin-prism/src/get-decorations.ts
@@ -1,11 +1,13 @@
 /* Copyright 2021, Milkdown by Mirone. */
 import { Decoration, DecorationSet, findChildren, Node } from '@milkdown/prose';
-import { highlight, RefractorNode } from 'refractor';
+import { highlight, listLanguages, RefractorNode } from 'refractor';
 
 export type FlattedNode = {
     text: string;
     className: string[];
 };
+
+const allLanguages = listLanguages();
 
 const flatNodes = (nodes: RefractorNode[], className: string[] = []) =>
     nodes.flatMap((node): FlattedNode[] =>
@@ -20,7 +22,10 @@ export function getDecorations(doc: Node, name: string) {
     findChildren((node) => node.type.name === name)(doc).forEach((block) => {
         let from = block.pos + 1;
         const { language } = block.node.attrs;
-        if (!language) return;
+        if (!language || !allLanguages.includes(language)) {
+            console.warn('Unsupported language detected, this language has not been supported by prism: ', language);
+            return;
+        }
         const nodes = highlight(block.node.textContent, language);
 
         flatNodes(nodes).forEach((node) => {
