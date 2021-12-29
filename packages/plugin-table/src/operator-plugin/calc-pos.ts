9125000290b6dee1930f4b8642aff2a4d299faf1
Mirone <Saul-Mirone@outlook.com>
2021-12-29 07:45:53 +0000

    fix: 🐛 toolbar and input position

--- packages/plugin-table/src/operator-plugin/calc-pos.ts
+++ packages/plugin-table/src/operator-plugin/calc-pos.ts
@@ -8,10 +8,14 @@ export const calculatePosition = (view: EditorView, dom: HTMLElement) => {
     const isRow = selection.isRowSelection();
 
     calculateNodePosition(view, dom, (selected, target, parent) => {
+        const $editor = dom.parentElement;
+        if (!$editor) {
+            throw new Error();
+        }
         let left = !isRow
             ? selected.left - parent.left + (selected.width - target.width) / 2
             : selected.left - parent.left - target.width / 2 - 8;
-        const top = selected.top - parent.top - target.height - (isCol ? 14 : 0) - 14;
+        const top = selected.top - parent.top - target.height - (isCol ? 14 : 0) - 14 + $editor.scrollTop;
 
         if (left < 0) {
             left = 0;
--- packages/plugin-tooltip/src/input-manager/calc-input-pos.ts
+++ packages/plugin-tooltip/src/input-manager/calc-input-pos.ts
@@ -3,9 +3,14 @@ import { calculateTextPosition, EditorView } from '@milkdown/prose';
 
 export const calcInputPos = (view: EditorView, input: HTMLDivElement) => {
     calculateTextPosition(view, input, (start, end, target, parent) => {
+        const $editor = input.parentElement;
+        if (!$editor) {
+            throw new Error();
+        }
+
         const selectionWidth = end.left - start.left;
         let left = start.left - parent.left - (target.width - selectionWidth) / 2;
-        const top = start.bottom - parent.top + 14;
+        const top = start.bottom - parent.top + 14 + $editor.scrollTop;
 
         if (left < 0) left = 0;
 
