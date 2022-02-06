42836d2231d4b3530bd7f3d760d496b9bdd35f38
Mirone Saul <Saul-Mirone@outlook.com>
2022-02-06 23:10:28 +0800

    fix: 🐛 type errors

--- packages/design-system/src/transformer.ts
+++ packages/design-system/src/transformer.ts
@@ -1,6 +1,4 @@
 /* Copyright 2021, Milkdown by Mirone. */
-import { Color, PR } from './types';
-
 type RGB = [number, number, number];
 export const hex2rgb = (hex: string): RGB | null => {
     const rgbShorthandRegex = /^([a-f\d])([a-f\d])([a-f\d])$/i;
@@ -15,42 +13,3 @@ export const hex2rgb = (hex: string): RGB | null => {
 
     return ok ? ([r, g, b].map(parse16) as RGB) : null;
 };
-
-export const themeColor = (hex: string) => {
-    if (hex.startsWith('rgba')) {
-        return hex.slice(5, -1).split(',').slice(0, -1).join(',');
-    }
-    if (hex.startsWith('rgb')) {
-        return hex.slice(4, -1);
-    }
-
-    const hex2rgb = (hex: string) => {
-        const rgbShorthandRegex = /^([a-f\d])([a-f\d])([a-f\d])$/i;
-        const rgbRegex = /^([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i;
-        const parse16 = (x: string) => parseInt(x, 16);
-
-        const fullHex = hex.slice(1).replace(rgbShorthandRegex, (_, r, g, b) => {
-            return r + r + g + g + b + b;
-        });
-
-        const [ok, r = '0', g = '0', b = '0'] = fullHex.match(rgbRegex) || [];
-
-        return ok ? [r, g, b].map(parse16) : null;
-    };
-
-    const rgb = hex2rgb(hex);
-    if (!rgb) {
-        console.warn(`Invalid hex: ${hex}`);
-        return hex;
-    }
-
-    return rgb.join(', ');
-};
-
-export const obj2var = <T extends string, U>(x: PR<T, U> = {}, transform = (x: U): string => `${x}`) =>
-    Object.entries(x)
-        .map(([k, v]) => {
-            return `--${k}: ${transform(v as U)};`;
-        })
-        .join('\n');
-export const obj2color = (x: PR<Color> = {}) => obj2var(x, themeColor);
