bf2f0c18e15cf4a70a43c4beb0b9920f0bc8786d
Mirone Saul <Saul-Mirone@outlook.com>
2022-02-13 16:18:25 +0800

    chore: make vite config can be merged

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,28 @@ export const rollupPlugins: Plugin[] = [autoExternal()];
 
 const resolvePath = (str: string) => path.resolve(__dirname, str);
 
+function isObject(item: unknown): item is Record<string, unknown> {
+    return Boolean(item && typeof item === 'object' && !Array.isArray(item));
+}
+
+function mergeDeep<T>(target: T, ...sources: T[]): T {
+    if (!sources.length) return target;
+    const source = sources.shift();
+
+    if (isObject(target) && isObject(source)) {
+        for (const key in source) {
+            if (isObject(source[key])) {
+                if (!target[key]) Object.assign(target, { [key]: {} });
+                mergeDeep(target[key] as T, source[key] as T);
+            } else {
+                Object.assign(target, { [key]: source[key] });
+            }
+        }
+    }
+
+    return mergeDeep(target, ...sources);
+}
+
 export const external = [
     'tslib',
     '@emotion/css',
@@ -53,28 +75,32 @@ export const external = [
     '@milkdown/plugin-upload',
 ];
 
-export const viteBuild = (packageDirName: string): BuildOptions => ({
-    sourcemap: true,
-    lib: {
-        entry: resolvePath(`packages/${packageDirName}/src/index.ts`),
-        name: `milkdown_${packageDirName}`,
-        fileName: libFileName,
-        formats: ['es'],
-    },
-    rollupOptions: {
-        external,
-        output: {
-            dir: resolvePath(`packages/${packageDirName}/lib`),
+export const viteBuild = (packageDirName: string, options: BuildOptions = {}): BuildOptions =>
+    mergeDeep<BuildOptions>(
+        {
+            sourcemap: true,
+            lib: {
+                entry: resolvePath(`packages/${packageDirName}/src/index.ts`),
+                name: `milkdown_${packageDirName}`,
+                fileName: libFileName,
+                formats: ['es'],
+            },
+            rollupOptions: {
+                external,
+                output: {
+                    dir: resolvePath(`packages/${packageDirName}/lib`),
+                },
+                plugins: rollupPlugins,
+            },
         },
-        plugins: rollupPlugins,
-    },
-});
+        options,
+    );
 
 export const pluginViteConfig = (packageDirName: string, options: ViteUserConfig = {}) => {
     const vitePlugins = options.plugins ?? [];
     return defineConfig({
-        build: viteBuild(packageDirName),
         ...options,
+        build: viteBuild(packageDirName, options.build),
         plugins: vitePlugins,
     });
 };
