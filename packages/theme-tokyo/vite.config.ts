1bd7eaf26149c0c6cfad3d116caf90fb2bed4444
Mirone <Saul-Mirone@outlook.com>
2022-02-20 05:12:49 +0000

    fix: 🐛 package name

--- packages/theme-tokyo/vite.config.ts
+++ packages/theme-tokyo/vite.config.ts
@@ -1,4 +1,4 @@
 /* Copyright 2021, Milkdown by Mirone. */
 import { pluginViteConfig } from '../../vite.config';
 
-export default pluginViteConfig('theme-nord');
+export default pluginViteConfig('theme-tokyo');
