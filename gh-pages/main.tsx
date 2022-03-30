5e871065179a9d8e7b22b366550b9c876c27dc27
Mirone <Saul-Mirone@outlook.com>
2022-03-30 07:42:40 +0000

    feat: 🎸 upgrade react

--- gh-pages/main.tsx
+++ gh-pages/main.tsx
@@ -2,10 +2,14 @@
 import './style.css';
 
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import { App } from './component/App';
 
 const root = document.getElementById('app');
 
-render(<App />, root);
+if (!root) {
+    throw new Error('Root element #app not found');
+}
+
+createRoot(root).render(<App />);
