ad6335657b1c8ef505fc79af447b3585c3b2302e
Mirone Saul <Saul-Mirone@outlook.com>
2022-06-12 00:44:00 +0800

    fix: 🐛 add a workaround for kleur and nuxt

--- packages/transformer/src/index.ts
+++ packages/transformer/src/index.ts
@@ -1,4 +1,13 @@
 /* Copyright 2021, Milkdown by Mirone. */
+
+/**
+ * This hack is meant to fix https://github.com/lukeed/kleur/pull/54
+ * kleur is a dependency of remark.
+ */
+
+if (process && !process.env) {
+    process.env = {};
+}
 export * from './parser';
 export * from './serializer';
 export * from './utility';
