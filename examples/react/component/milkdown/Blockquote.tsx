d6e6249d8bf29be7fa579fbbc9ef7f225c432d02
Mirone Saul <Saul-Mirone@outlook.com>
2022-06-16 01:02:31 +0800

    refactor: 💡 use react-jsx

--- examples/react/component/milkdown/Blockquote.tsx
+++ examples/react/component/milkdown/Blockquote.tsx
@@ -1,6 +1,6 @@
 /* Copyright 2021, Milkdown by Mirone. */
-import React, { ReactNode } from 'react';
+import { FC, ReactNode } from 'react';
 
-export const Blockquote: React.FC<{ children: ReactNode }> = ({ children }) => {
+export const Blockquote: FC<{ children: ReactNode }> = ({ children }) => {
     return <div className="react-renderer blockquote">{children}</div>;
 };
--- examples/react/component/milkdown/Image.tsx
+++ examples/react/component/milkdown/Image.tsx
@@ -1,8 +1,16 @@
 /* Copyright 2021, Milkdown by Mirone. */
 import { useNodeCtx } from '@milkdown/react';
-import React from 'react';
+import { FC } from 'react';
 
-export const Image: React.FC = () => {
+export const Image: FC = () => {
     const { node } = useNodeCtx();
-    return <img className="image" src={node.attrs['src']} alt={node.attrs['alt']} title={node.attrs['tittle']} />;
+    return (
+        <img
+            className="image"
+            style={{ border: '1px solid red' }}
+            src={node.attrs['src']}
+            alt={node.attrs['alt']}
+            title={node.attrs['tittle']}
+        />
+    );
 };
