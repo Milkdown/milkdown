b625cdb75f3b13b2a34741ef68703b1f8f3d2717
Mirone <Saul-Mirone@outlook.com>
2022-07-08 09:41:48 +0000

    feat: 🎸 calculate block handle position

---
+++ packages/plugin-block/src/create-block-handle.ts
@@ -0,0 +1,30 @@
+/* Copyright 2021, Milkdown by Mirone. */
+
+import { ThemeIcon } from '@milkdown/core';
+import { Utils } from '@milkdown/utils';
+
+export const createBlockHandle = ({ themeManager, getStyle }: Utils) => {
+    const dom = document.createElement('div');
+    const icon = themeManager.get(ThemeIcon, 'dragHandle');
+
+    dom.appendChild(icon.dom);
+    themeManager.onFlush(() => {
+        if (!dom) return;
+
+        const style = getStyle(({ css }) => {
+            return css`
+                position: absolute;
+                &.hide {
+                    display: none;
+                }
+            `;
+        });
+
+        if (style) {
+            const className = ['block-handle', style, 'hide'].filter((x): x is string => x != null).join(' ');
+            dom.className = className;
+        }
+    });
+
+    return dom;
+};
