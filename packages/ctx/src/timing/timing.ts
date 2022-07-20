5db027c7b08587d0a5a095a9bc945467825b1bb3
z-950 <1033766950@qq.com>
2022-07-20 18:18:42 +0800

    fix: 🐛 memory leak of timing listener

--- packages/ctx/src/timing/timing.ts
+++ packages/ctx/src/timing/timing.ts
@@ -10,34 +10,30 @@ export const createTimer = (name: string, timeout = 3000): Timer => {
     const id = Symbol('Timer');
 
     const timer = (store: ClockMap) => {
-        let isResolved = false;
+        let promise: Promise<void> | null = null;
         let listener: EventListener;
 
         const data = Symbol(name);
 
         const timing: Timing = () =>
-            isResolved
-                ? Promise.resolve(undefined)
-                : new Promise((resolve, reject) => {
-                      listener = (e: Event) => {
-                          if (!(e instanceof CustomEvent)) {
-                              return;
-                          }
-                          if (e.detail.id === data) {
-                              isResolved = true;
-                              resolve();
-                          }
-                      };
-                      setTimeout(() => {
-                          reject(`Timing ${name} timeout.`);
-                          removeEventListener(name, listener);
-                      }, timeout);
-                      addEventListener(name, listener);
-                  });
+            (promise ??= new Promise((resolve, reject) => {
+                listener = (e: Event) => {
+                    if (!(e instanceof CustomEvent)) {
+                        return;
+                    }
+                    if (e.detail.id === data) {
+                        resolve();
+                    }
+                };
+                setTimeout(() => {
+                    reject(`Timing ${name} timeout.`);
+                    removeEventListener(name, listener);
+                }, timeout);
+                addEventListener(name, listener, { once: true });
+            }));
         timing.done = () => {
             const event = new CustomEvent(name, { detail: { id: data } });
             dispatchEvent(event);
-            removeEventListener(name, listener);
         };
 
         store.set(id, timing);
