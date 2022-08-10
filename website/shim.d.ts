dc3ed87f39563702d6fb1de20b60e2238ba666cc
Mirone <Saul-Mirone@outlook.com>
2022-08-10 09:02:33 +0000

    feat: 🎸 use lz to compress share url
    ✅ Closes: #664

--- website/shim.d.ts
+++ website/shim.d.ts
@@ -1,2 +1,19 @@
 /* Copyright 2021, Milkdown by Mirone. */
 declare let docsearch: (options: unknown) => void;
+
+declare module 'lz-string' {
+    export function compressToBase64(input: string): string;
+    export function decompressFromBase64(input: string): string;
+
+    export function compressToUTF16(input: string): string;
+    export function decompressFromUTF16(compressed: string): string;
+
+    export function compressToUint8Array(uncompressed: string): Uint8Array;
+    export function decompressFromUint8Array(compressed: Uint8Array): string;
+
+    export function compressToEncodedURIComponent(input: string): string;
+    export function decompressFromEncodedURIComponent(compressed: string): string;
+
+    export function compress(input: string): string;
+    export function decompress(compressed: string): string;
+}
--- website/utils/share.ts
+++ website/utils/share.ts
@@ -1,33 +1,6 @@
 /* Copyright 2021, Milkdown by Mirone. */
+import { compressToBase64, decompressFromBase64 } from 'lz-string';
 
-const toBinary = (string: string): string => {
-    const codeUnits = new Uint16Array(string.length);
-    for (let i = 0; i < codeUnits.length; i++) {
-        codeUnits[i] = string.charCodeAt(i);
-    }
-    const charCodes = new Uint8Array(codeUnits.buffer);
-    let result = '';
-    for (let i = 0; i < charCodes.byteLength; i++) {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        result += String.fromCharCode(charCodes[i]!);
-    }
-    return result;
-};
+export const encode = compressToBase64;
 
-export const encode = (string: string): string => btoa(toBinary(string));
-
-const fromBinary = (binary: string): string => {
-    const bytes = new Uint8Array(binary.length);
-    for (let i = 0; i < bytes.length; i++) {
-        bytes[i] = binary.charCodeAt(i);
-    }
-    const charCodes = new Uint16Array(bytes.buffer);
-    let result = '';
-    for (let i = 0; i < charCodes.length; i++) {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        result += String.fromCharCode(charCodes[i]!);
-    }
-    return result;
-};
-
-export const decode = (string: string): string => fromBinary(atob(string));
+export const decode = decompressFromBase64;
