8700335ab811e9126e0226591287efd6e404d794
Mirone Saul <Saul-Mirone@outlook.com>
2022-08-29 20:57:28 +0800

    feat: 🎸 optimize code

--- packages/preset-commonmark/src/plugin/inline-sync.ts
+++ packages/preset-commonmark/src/plugin/inline-sync.ts
@@ -1,15 +1,7 @@
 /* Copyright 2021, Milkdown by Mirone. */
 import { Ctx, editorViewCtx, parserCtx, serializerCtx } from '@milkdown/core';
-import { Node } from '@milkdown/prose/model';
-import { Plugin, PluginKey, TextSelection } from '@milkdown/prose/state';
-
-type InlineSyncPluginState = {
-    text: string;
-    isInlineBlock: boolean;
-    originalNode: Node | null;
-    targetNode: Node | null;
-};
-type InlineSyncPlugin = Plugin<InlineSyncPluginState>;
+import { Attrs, Node } from '@milkdown/prose/model';
+import { EditorState, Plugin, PluginKey, TextSelection, Transaction } from '@milkdown/prose/state';
 
 const placeholder = '⁂';
 const regexp = new RegExp(`\\\\(?=[^\\w\\s${placeholder}\\\\]|_)`, 'g');
@@ -28,7 +20,7 @@ const movePlaceholder = (text: string) => {
     const symbolsNeedToMove = ['*', '_'];
 
     let index = text.indexOf(placeholder);
-    while (symbolsNeedToMove.includes(text[index - 1] || '') && symbolsNeedToMove.includes(text[index + 1] || '')) {
+    while (symbolsNeedToMove.includes(text[index + 1] || '')) {
         text = swap(text, index, index + 1);
         index = index + 1;
     }
@@ -36,130 +28,144 @@ const movePlaceholder = (text: string) => {
     return text;
 };
 
+const getOffset = (node: Node, from: number) => {
+    let offset = from;
+    let find = false;
+    node.descendants((n) => {
+        if (find) return false;
+        if (n.isText) {
+            const i = n.text?.indexOf(placeholder);
+            if (i != null && i >= 0) {
+                find = true;
+                offset += i;
+                return false;
+            }
+        }
+        offset += n.nodeSize;
+        return;
+    });
+    return offset;
+};
+
 export const inlineSyncPluginKey = new PluginKey('MILKDOWN_INLINE_SYNC');
 export const getInlineSyncPlugin = (ctx: Ctx) => {
-    const getOffset = (node: Node, from: number) => {
-        let offset = from;
-        let find = false;
-        node.descendants((n) => {
-            if (find) return false;
-            if (n.isText) {
-                const i = n.text?.indexOf(placeholder);
-                if (i != null && i >= 0) {
-                    find = true;
-                    offset += i;
-                    return false;
-                }
-            }
-            offset += n.nodeSize;
-            return;
-        });
-        return offset;
+    const getContextByState = (state: EditorState) => {
+        const { selection } = state;
+        const { $from } = selection;
+
+        const node = $from.node();
+        const doc = state.schema.topNodeType.create(undefined, node);
+        const isInlineBlock = Boolean(node.type.spec.content?.includes('inline'));
+
+        const parser = ctx.get(parserCtx);
+        const serializer = ctx.get(serializerCtx);
+        const text = serializer(doc).slice(0, -1).replaceAll(regexp, '');
+        const parsed = parser(movePlaceholder(text));
+        if (!parsed) return null;
+
+        const target = parsed.firstChild;
+
+        if (!target || node.type !== target.type) return null;
+
+        return {
+            text,
+            isInlineBlock,
+            prevNode: node,
+            nextNode: target,
+        };
     };
 
-    const getInitialState = (): InlineSyncPluginState => ({
-        isInlineBlock: false,
-        originalNode: null,
-        targetNode: null,
-        text: '',
-    });
+    const runReplacer = (
+        prevState: EditorState,
+        state: EditorState,
+        dispatch: (tr: Transaction) => void,
+        attrs: Attrs,
+    ) => {
+        const { selection } = state;
+        const { $from } = selection;
+        const from = $from.before();
+        const to = $from.after();
 
-    const inlineSyncPlugin: InlineSyncPlugin = new Plugin<InlineSyncPluginState>({
+        let tr = state.tr.setMeta(inlineSyncPluginKey, true).insertText(placeholder, selection.from);
+        const cleanUpOnFail = () => {
+            const offset = getOffset($from.node(), from);
+            dispatch(tr.delete(offset + 1, offset + 2));
+        };
+
+        // If doc structure is changed, remove the placeholder.
+        if (prevState.doc.resolve(from).node().type !== state.doc.resolve(from).node().type) {
+            let offset = 0;
+            let find = false;
+            state.doc.descendants((n, pos) => {
+                if (find) return false;
+                if (n.isText && n.text === placeholder) {
+                    find = true;
+                    offset = pos;
+                }
+                return;
+            });
+            if (!find) return;
+
+            dispatch(tr.delete(offset, offset + 1));
+
+            return;
+        }
+
+        const context = getContextByState(state.apply(tr));
+
+        if (!context) {
+            cleanUpOnFail();
+            return;
+        }
+
+        const offset = getOffset(context.nextNode, from);
+
+        tr = tr.replaceWith(from, to, context.nextNode);
+        tr = tr.setNodeMarkup(from, undefined, attrs);
+        tr = tr.delete(offset + 1, offset + 2);
+        tr = tr.setSelection(TextSelection.near(tr.doc.resolve(offset + 1)));
+        dispatch(tr);
+    };
+
+    const inlineSyncPlugin = new Plugin<null>({
         key: inlineSyncPluginKey,
         state: {
             init: () => {
-                return getInitialState();
+                return null;
             },
-            apply: (_tr, _value, _oldState, state) => {
-                const { selection } = state;
+            apply: (tr, _value, _oldState, newState) => {
+                if (!tr.docChanged) return null;
+
+                const meta = tr.getMeta(inlineSyncPluginKey);
+                if (meta) return null;
+
+                const { selection } = tr;
                 const { $from } = selection;
 
-                const node = $from.node();
-                const doc = state.schema.topNodeType.create(undefined, node);
-                const isInlineBlock = Boolean(node.type.spec.content?.includes('inline'));
+                const prevNode = $from.node();
+                const doc = newState.schema.topNodeType.create(undefined, prevNode);
+                const isInlineBlock = Boolean(prevNode.type.spec.content?.includes('inline'));
+                if (!isInlineBlock) return null;
 
                 const parser = ctx.get(parserCtx);
                 const serializer = ctx.get(serializerCtx);
                 const text = serializer(doc).slice(0, -1).replaceAll(regexp, '');
                 const parsed = parser(movePlaceholder(text));
-                if (!parsed) return getInitialState();
 
-                const target = parsed.firstChild;
+                if (!parsed) return null;
 
-                if (!target || node.type !== target.type) return getInitialState();
+                const nextNode = parsed.firstChild;
 
-                return {
-                    text,
-                    isInlineBlock,
-                    originalNode: node,
-                    targetNode: target,
-                };
+                if (!nextNode || prevNode.type !== nextNode.type) return null;
+
+                requestAnimationFrame(() => {
+                    const { dispatch, state } = ctx.get(editorViewCtx);
+
+                    runReplacer(newState, state, dispatch, prevNode.attrs);
+                });
+
+                return null;
             },
-        },
-        props: {
-            handleTextInput: (view, _, to) => {
-                if (view.composing) return false;
-                const pluginState = inlineSyncPlugin.getState(view.state);
-                if (pluginState?.isInlineBlock) {
-                    view.dispatch(view.state.tr.setMeta(inlineSyncPluginKey, true).insertText(placeholder, to));
-                }
-                return false;
-            },
-        },
-        appendTransaction(transactions, _oldState, newState) {
-            const triggerTr = transactions.find((tr) => tr.getMeta(inlineSyncPluginKey));
-            if (!triggerTr) return null;
-
-            requestAnimationFrame(() => {
-                const { state, dispatch } = ctx.get(editorViewCtx);
-
-                const { selection } = state;
-                const { $from } = selection;
-                const from = $from.before();
-                const to = $from.after();
-
-                const cleanUpOnFail = () => {
-                    const offset = getOffset($from.node(), from);
-                    dispatch(state.tr.delete(offset + 1, offset + 2));
-                };
-
-                // If doc structure is changed, remove the placeholder.
-                if (newState.doc.resolve(from).node().type !== state.doc.resolve(from).node().type) {
-                    let offset = 0;
-                    let find = false;
-                    state.doc.descendants((n, pos) => {
-                        if (find) return false;
-                        if (n.isText && n.text === placeholder) {
-                            find = true;
-                            offset = pos;
-                        }
-                        return;
-                    });
-                    dispatch(state.tr.delete(offset, offset + 1));
-
-                    return;
-                }
-
-                const pluginState = inlineSyncPlugin.getState(state);
-                if (!pluginState) {
-                    cleanUpOnFail();
-                    return;
-                }
-
-                const { targetNode, originalNode } = pluginState;
-                if (!targetNode || !originalNode) {
-                    cleanUpOnFail();
-                    return;
-                }
-
-                const offset = getOffset(targetNode, from);
-
-                let tr = state.tr.replaceWith(from, to, targetNode);
-                tr = tr.delete(offset + 1, offset + 2);
-                tr = tr.setSelection(TextSelection.near(tr.doc.resolve(offset + 1)));
-                dispatch(tr);
-            });
-            return null;
         },
     });
 
