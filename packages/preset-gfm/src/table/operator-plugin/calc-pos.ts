c84cdad15d33406d2654adf479f7fba2b218fedb
Xiaodong Chen <ihapboy@gmail.com>
2022-11-08 19:02:04 +0800

    fix: 🐛 tooltip display overflow (#796)
    ✅ Closes: #795

--- packages/preset-gfm/src/table/operator-plugin/calc-pos.ts
+++ packages/preset-gfm/src/table/operator-plugin/calc-pos.ts
@@ -19,11 +19,22 @@ export const calculatePosition = (view: EditorView, dom: HTMLElement) => {
         let left = !isRow
             ? selected.left - parent.left + (selected.width - target.width) / 2
             : selected.left - parent.left - target.width / 2 - 8;
-        const top = selected.top - parent.top - target.height - (isCol ? 14 : 0) - 14 + $editor.scrollTop;
+        let top = selected.top - parent.top - target.height - (isCol ? 14 : 0) - 14 + $editor.scrollTop;
 
         if (left < 0) {
             left = 0;
         }
+
+        const scrollbarWidth = $editor.offsetWidth - $editor.clientWidth;
+        const maxLeft = parent.width - scrollbarWidth - (target.width + 4);
+        if (left > maxLeft) {
+            left = maxLeft;
+        }
+
+        if (top < $editor.scrollTop) {
+            top = selected.top - parent.top + 14 + $editor.scrollTop;
+        }
+
         return [top, left];
     });
 };
