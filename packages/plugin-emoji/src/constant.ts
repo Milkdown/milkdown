9bab0bc6aae7379e74db70ada713cc17b311942f
Mirone Saul <Saul-Mirone@outlook.com>
2022-11-27 21:53:34 +0800

    feat: 🎸 migrate math plugin

--- packages/plugin-emoji/src/constant.ts
+++
@@ -1,4 +0,0 @@
-/* Copyright 2021, Milkdown by Mirone. */
-export const part = /:\+1|:-1|:[\w-]+/
-export const full = /:\+1:|:-1:|:[\w-]+:/
-export const input = /(:([^:\s]+):)$/
--- packages/plugin-math/src/nodes.ts
+++
@@ -1,9 +0,0 @@
-/* Copyright 2021, Milkdown by Mirone. */
-import { AtomList } from '@milkdown/utils'
-
-import { mathBlock } from './math-block'
-import { mathInline } from './math-inline'
-
-export const nodes = AtomList.create([mathInline(), mathBlock()])
-export * from './math-block'
-export * from './math-inline'
