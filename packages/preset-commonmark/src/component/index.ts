7c8b9fe7f1457e88c00bd8a83a3343b129196453
Mirone Saul <Saul-Mirone@outlook.com>
2022-12-04 20:18:17 +0800

    feat: 🎸 remove component in preset

--- packages/preset-commonmark/src/component/index.ts
+++
@@ -1,6 +0,0 @@
-/* Copyright 2021, Milkdown by Mirone. */
-import { codeBlockFrame, codeBlockFrameConfig } from './code-block-frame'
-
-export * from './code-block-frame'
-
-export const components = [codeBlockFrame, codeBlockFrameConfig].flat()
