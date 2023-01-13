1ed255e1d6f3aa9a6ae230bf660c9b8ba444d0ec
Mirone Saul <Saul-Mirone@outlook.com>
2023-01-13 23:56:12 +0800

    fix: 🐛 maxcdn for twemoji has shut down
    ✅ Closes: #876

--- packages/plugin-emoji/src/parse.ts
+++ packages/plugin-emoji/src/parse.ts
@@ -4,4 +4,4 @@ import twemoji from 'twemoji'
 const setAttr = (text: string) => ({ title: text })
 
 export const parse = (emoji: string, twemojiOptions?: TwemojiOptions): string =>
-  twemoji.parse(emoji, { attributes: setAttr, ...twemojiOptions }) as unknown as string
+  twemoji.parse(emoji, { attributes: setAttr, base: 'https://cdn.jsdelivr.net/gh/twitter/twemoji/assets/', ...twemojiOptions }) as unknown as string
