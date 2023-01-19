e89655f801ac839b40fd13a15fe638145e0e5720
Mirone Saul <Saul-Mirone@outlook.com>
2023-01-19 17:48:42 +0800

    feat: 🎸 add  API doc for history plugin

--- packages/plugin-history/src/index.ts
+++ packages/plugin-history/src/index.ts
@@ -4,15 +4,25 @@ import { commandsCtx, createCmdKey } from '@milkdown/core'
 import { history as prosemirrorHistory, redo, undo } from '@milkdown/prose/history'
 import { $command, $ctx, $prose, $useKeymap } from '@milkdown/utils'
 
+/// The milkdown command key for `Undo` command.
 export const Undo = createCmdKey('Undo')
+
+/// The milkdown command key for `Redo` command.
 export const Redo = createCmdKey('Redo')
 
+/// The milkdown command wrapper of [undo API](https://prosemirror.net/docs/ref/#history.undo) in [prosemirror-history](https://prosemirror.net/docs/ref/#history).
 export const undoCommand = $command('Undo', () => () => undo)
+
+/// The milkdown command wrapper of [redo API](https://prosemirror.net/docs/ref/#history.redo) in [prosemirror-history](https://prosemirror.net/docs/ref/#history).
 export const redoCommand = $command('Undo', () => () => redo)
 
+/// The [config](https://prosemirror.net/docs/ref/#history.history%5Econfig) of prosemirror history plugin.
 export const historyProviderConfig = $ctx<{ depth?: number; newGroupDelay?: number }, 'historyProviderConfig'>({}, 'historyProviderConfig')
+
+/// The milkdown wrapper of [history API](https://prosemirror.net/docs/ref/#history.history) in [prosemirror-history](https://prosemirror.net/docs/ref/#history).
 export const historyProviderPlugin = $prose(ctx => prosemirrorHistory(ctx.get(historyProviderConfig.key)))
 
+/// The keymap of history plugin, it's `mod-z` for undo and `mod-y`/`shift-mod-z` for redo.
 export const historyKeymap = $useKeymap('historyKeymap', {
   Undo: {
     shortcuts: 'Mod-z',
@@ -30,4 +40,5 @@ export const historyKeymap = $useKeymap('historyKeymap', {
   },
 })
 
+/// The milkdown history plugin.
 export const history: MilkdownPlugin[] = [historyProviderConfig, historyProviderPlugin, historyKeymap, undoCommand, redoCommand].flat()
--- website/pages/api/plugin-history/index.md
+++ website/pages/api/plugin-history/index.md
@@ -2,6 +2,8 @@
 
 History undo & redo support for [milkdown](https://milkdown.dev/).
 
+## Usage
+
 ```typescript
 import { Editor } from '@milkdown/core';
 import { commonmark } from '@milkdown/preset-commonmark';
@@ -10,4 +12,52 @@ import { nord } from '@milkdown/theme-nord';
 import { history } from '@milkdown/plugin-history';
 
 Editor.make().use(nord).use(commonmark).use(history).create();
+```
+
+## Plugin
+
+@history
+@historyProviderConfig
+@historyProviderPlugin
+
+## Keymap
+
+@historyKeymap
+
+You can remap the keymap by using the `historyKeymap.key`.
+
+```typescript
+import { history, historyKeymap } from '@milkdown/plugin-history';
+
+Editor.make()
+    .config((ctx) => {
+      ctx.set(historyKeymap.key, {
+        // Remap to one shortcut.
+        Undo: 'Mod-z',
+        // Remap to multiple shortcuts.
+        Redo: ['Mod-y', 'Shift-Mod-z'],
+      })
+    })
+    .use(nord)
+    .use(commonmark)
+    .use(history)
+    .create();
+```
+
+## Commands
+
+@Undo
+@Redo
+@undoCommand
+@redoCommand
+
+You can call the commands programmatically.
+
+```typescript
+import { Undo, history } from '@milkdown/plugin-history';
+import { callCommand } from '@milkdown/plugin-utils';
+
+const editor = await Editor.make().use(/* ... */).use(history).create();
+
+editor.action(callCommand(Undo))
 ```
