d3d2cb9b9870c05347181ee66eb69501a57740e9
Mirone Saul <Saul-Mirone@outlook.com>
2023-01-23 13:49:33 +0800

    feat: 🎸 align vue api with react

--- packages/vue/src/useEditor.ts
+++ packages/vue/src/useEditor.ts
@@ -1,24 +1,17 @@
 /* Copyright 2021, Milkdown by Mirone. */
 
-import type { Editor } from '@milkdown/core'
-import { ref } from 'vue'
+import { inject } from 'vue'
+import { editorInfoCtxKey } from './Editor'
 
 import type { GetEditor, UseEditorReturn } from './types'
 
 export const useEditor = (getEditor: GetEditor): UseEditorReturn => {
-  const dom = ref<HTMLDivElement | null>(null)
-  const editor = ref<Editor>()
-  const loading = ref(true)
-  const getEditorCallback = ref<GetEditor>((...args) => getEditor(...args))
+  const { editorFactory, loading, editor } = inject(editorInfoCtxKey)!
+
+  editorFactory.value = getEditor
 
   return {
     loading,
     get: () => editor.value,
-    editor: {
-      getEditorCallback,
-      dom,
-      editor,
-      loading,
-    },
   }
 }
