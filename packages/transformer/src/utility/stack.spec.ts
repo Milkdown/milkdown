da6e265f589e2bee5a399de08b11ddb532ba9e03
Mirone Saul <Saul-Mirone@outlook.com>
2023-01-29 17:14:03 +0800

    test: 💍 add unit test for stack

---
+++ packages/transformer/src/utility/stack.spec.ts
@@ -0,0 +1,30 @@
+/* Copyright 2021, Milkdown by Mirone. */
+import { expect, it } from 'vitest'
+import { Stack } from './stack'
+
+it('stack', () => {
+  const stack = new Stack<number, Array<number>>()
+
+  expect(stack.size()).toBe(0)
+
+  stack.open([])
+  stack.push(1)
+
+  expect(stack.size()).toBe(1)
+  expect(stack.top()).toEqual([1])
+
+  stack.open([])
+  stack.push(2)
+
+  expect(stack.size()).toBe(2)
+  expect(stack.top()).toEqual([2])
+
+  stack.push(3)
+  const a = stack.close()
+  expect(a).toEqual([2, 3])
+  expect(stack.size()).toBe(1)
+
+  const b = stack.close()
+  expect(b).toEqual([1])
+  expect(stack.size()).toBe(0)
+})
