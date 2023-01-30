02df8f077e4f548bf8c00ebb663f8aaedf97da7e
Mirone Saul <Saul-Mirone@outlook.com>
2023-01-30 21:11:02 +0800

    test: 💍 add test for serializer stack element

---
+++ packages/transformer/src/serializer/stack-element.spec.ts
@@ -0,0 +1,17 @@
+/* Copyright 2021, Milkdown by Mirone. */
+import { expect, it } from 'vitest'
+import { SerializerStackElement } from './stack-element'
+
+it('serializer-stack-element', () => {
+  const element = SerializerStackElement.create('type')
+
+  expect(element.props).toEqual({})
+
+  element.push({ type: 'text' })
+
+  expect(element.children).toEqual([{ type: 'text' }])
+
+  const node = element.pop()
+
+  expect(node).toEqual({ type: 'text' })
+})
