1ad10495bb49bb53b658df032a2702103615646d
Mirone Saul <Saul-Mirone@outlook.com>
2023-02-09 10:52:32 +0800

    docs: ✏️ add documentation for prism package

--- packages/plugin-prism/src/index.ts
+++ packages/plugin-prism/src/index.ts
@@ -9,14 +9,18 @@ import { $ctx, $prose } from '@milkdown/utils'
 import type { MilkdownPlugin } from '@milkdown/ctx'
 import { getDecorations } from './get-decorations'
 
+/// @internal
 export interface Options {
   configureRefractor: (refractor: Refractor) => void | Refractor
 }
 
+/// Config for prism.
+/// You can configure refractor here.
 export const prismConfig = $ctx<Options, 'prismConfig'>({
   configureRefractor: () => {},
 }, 'prismConfig')
 
+/// The prism prosemirror plugin.
 export const prismPlugin = $prose((ctx) => {
   const { configureRefractor } = ctx.get(prismConfig.key)
   const name = 'code_block'
@@ -32,22 +36,21 @@ export const prismPlugin = $prose((ctx) => {
         const isPreviousNodeName = oldState.selection.$head.parent.type.name === name
         const oldNode = findChildren(node => node.type.name === name)(oldState.doc)
         const newNode = findChildren(node => node.type.name === name)(state.doc)
-        const codeBlockChanged
-                    = transaction.docChanged
-                    && (isNodeName
-                        || isPreviousNodeName
-                        || oldNode.length !== newNode.length
-                        || oldNode[0]?.node.attrs.language !== newNode[0]?.node.attrs.language
-                        || transaction.steps.some((step) => {
-                          const s = step as unknown as { from: number; to: number }
-                          return (
-                            s.from !== undefined
-                                && s.to !== undefined
-                                && oldNode.some((node) => {
-                                  return node.pos >= s.from && node.pos + node.node.nodeSize <= s.to
-                                })
-                          )
-                        }))
+        const codeBlockChanged = transaction.docChanged
+        && (isNodeName
+        || isPreviousNodeName
+        || oldNode.length !== newNode.length
+        || oldNode[0]?.node.attrs.language !== newNode[0]?.node.attrs.language
+        || transaction.steps.some((step) => {
+          const s = step as unknown as { from: number; to: number }
+          return (
+            s.from !== undefined
+            && s.to !== undefined
+            && oldNode.some((node) => {
+              return node.pos >= s.from && node.pos + node.node.nodeSize <= s.to
+            })
+          )
+        }))
 
         if (codeBlockChanged)
           return getDecorations(transaction.doc, name, refractor)
@@ -63,4 +66,5 @@ export const prismPlugin = $prose((ctx) => {
   })
 })
 
+/// All plugins exported by this package.
 export const prism: MilkdownPlugin[] = [prismPlugin, prismConfig]
--- website/pages/api/plugin-prism/index.md
+++ website/pages/api/plugin-prism/index.md
@@ -2,17 +2,70 @@
 
 Prism plugin for [milkdown](https://milkdown.dev/).
 Add support for prism highlight.
+This package uses [refractor](https://www.npmjs.com/package/refractor) so it has the same support and limitation as refractor.
 
-> ⚠️ Keep in mind that you need to import prism style on your own.
+> **From the refractor README:**
 >
-> For example, using [prism-themes](https://www.npmjs.com/package/prism-themes).
+> Only the custom built syntaxes in refractor/lang/*.js will work with refractor as
+> Prism’s own syntaxes are made to work with global variables and are not importable.
+>
+> Refractor also does not support Prism plugins,
+> due to the same limitations,
+> and that they almost exclusively deal with the DOM.
+
+## Usage
 
 ```typescript
 import { Editor } from '@milkdown/core';
 import { commonmark } from '@milkdown/preset-commonmark';
-import { nord } from '@milkdown/theme-nord';
 
 import { prism } from '@milkdown/plugin-prism';
 
-Editor.make().use(nord).use(commonmark).use(prism).create();
+Editor.make().use(commonmark).use(prism).create();
+```
+
+⚠️ Keep in mind that you need to import prism style on your own.
+
+For example, using [prism-themes](https://www.npmjs.com/package/prism-themes).
+
+```typescript
+import 'prism-themes/themes/prism-nord.css'
+```
+
+@prism
+
+@prismConfig
+@prismPlugin
+
+## Register Languages
+
+By default, refractor will not register any languages.
+You can register languages by yourself.
+
+```typescript
+import { prism, prismConfig } from '@milkdown/plugin-prism';
+
+import markdown from 'refractor/lang/markdown'
+import css from 'refractor/lang/css'
+import javascript from 'refractor/lang/javascript'
+import typescript from 'refractor/lang/typescript'
+import jsx from 'refractor/lang/jsx'
+import tsx from 'refractor/lang/tsx'
+
+Editor
+  .make()
+  .config((ctx) => {
+    ctx.set(prismConfig.key, {
+      configureRefractor: (refractor) => {
+        refractor.register(markdown)
+        refractor.register(css)
+        refractor.register(javascript)
+        refractor.register(typescript)
+        refractor.register(jsx)
+        refractor.register(tsx)
+      },
+    })
+  })
+  .use(prism)
+  .create();
 ```
