500f89d2031b5d8aced936350850595322ce142b
Mirone <Saul-Mirone@outlook.com>
2023-02-27 15:47:55 +0800

    feat: 🎸 add inputrule for task list (#908)

--- packages/preset-gfm/src/composed/inputrules.ts
+++ packages/preset-gfm/src/composed/inputrules.ts
@@ -1,8 +1,9 @@
 /* Copyright 2021, Milkdown by Mirone. */
 import type { MilkdownPlugin } from '@milkdown/ctx'
-import { insertTableInputRule } from '../node'
+import { insertTableInputRule, wrapInTaskListInputRule } from '../node'
 
 /// @internal
 export const inputrules: MilkdownPlugin[] = [
   insertTableInputRule,
+  wrapInTaskListInputRule,
 ]
