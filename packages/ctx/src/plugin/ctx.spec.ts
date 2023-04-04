969af945a77b4a202bc3d6f8852077849926b5a3
Mirone <Saul-Mirone@outlook.com>
2023-04-04 16:39:44 +0800

    test: 💍 add unit test for ctx produce

--- packages/ctx/src/plugin/ctx.spec.ts
+++ packages/ctx/src/plugin/ctx.spec.ts
@@ -5,6 +5,8 @@ import { Container, createSlice } from '../context'
 import { Clock, createTimer } from '../timer'
 import { Ctx } from './ctx'
 
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
 describe('ctx', () => {
   const container = new Container()
   const clock = new Clock()
@@ -47,5 +49,53 @@ describe('ctx', () => {
     }, 10)
 
     await expect(ctx.wait(timerType)).resolves.toBeUndefined()
+  })
+
+  it('produce', async () => {
+    const same = ctx.produce()
+    expect(same).toBe(ctx)
+
+    const different = ctx.produce({ displayName: 'test', package: 'test' })
+    expect(different).not.toBe(ctx)
+
+    const sliceType = createSlice('Foo', 'message')
+    different.inject(sliceType)
+
+    expect(same.get(sliceType)).toBe('Foo')
+    expect(different.inspector?.read().injectedSlices[0]).toEqual({ name: 'message', value: 'Foo' })
+
+    expect(ctx.inspector?.read().injectedSlices).toBeUndefined()
+
+    const sliceType2 = createSlice(true, 'boolean')
+    same.inject(sliceType2)
+
+    expect(different.get(sliceType2)).toBe(true)
+    expect(different.inspector?.read().consumedSlices[0]).toEqual({ name: 'boolean', value: true })
+
+    const timerType1 = createTimer('timer1')
+    different.record(timerType1)
+
+    setTimeout(() => {
+      different.done(timerType1)
+    }, 20)
+
+    await expect(ctx.wait(timerType1)).resolves.toBeUndefined()
+
+    expect(different.inspector?.read().recordedTimers[0].name).toBe('timer1')
+    expect(different.inspector?.read().recordedTimers[0].duration).not.toBeLessThan(20)
+    expect(different.inspector?.read().recordedTimers[0].status).toBe('resolved')
+
+    const timerType2 = createTimer('timer2')
+    ctx.record(timerType2)
+
+    setTimeout(() => {
+      ctx.done(timerType2)
+    }, 10)
+
+    await expect(different.wait(timerType2)).resolves.toBeUndefined()
+
+    expect(different.inspector?.read().waitTimers[0].name).toBe('timer2')
+    expect(different.inspector?.read().waitTimers[0].duration).not.toBeLessThan(10)
+    expect(different.inspector?.read().waitTimers[0].status).toBe('resolved')
   })
 })
