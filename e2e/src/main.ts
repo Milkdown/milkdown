c4240d299471bc705e83d724e9f3e6342fb79d8f
Mirone <Saul-Mirone@outlook.com>
2023-04-22 19:06:10 +0800

    chore: fix e2e

--- e2e/src/main.ts
+++ e2e/src/main.ts
@@ -50,23 +50,16 @@ const main = async () => {
     })
   globalThis.__inspect__ = () => editor!.inspect()
 
-  if (import.meta.env.PROD) {
-    const ui = document.querySelector<HTMLElement>('#ui')!
-    ui.remove()
+  const logButton = document.querySelector<HTMLDivElement>('#log')
+  if (logButton) {
+    // eslint-disable-next-line no-console
+    logButton.onclick = () => console.log(globalThis.__getMarkdown__())
   }
-  else {
-    const logButton = document.querySelector<HTMLDivElement>('#log')
 
-    if (logButton) {
-      // eslint-disable-next-line no-console
-      logButton.onclick = () => console.log(globalThis.__getMarkdown__())
-    }
-
-    const inspectButton = document.querySelector<HTMLDivElement>('#inspect')
-    if (inspectButton) {
-      // eslint-disable-next-line no-console
-      inspectButton.onclick = () => console.log(globalThis.__inspect__())
-    }
+  const inspectButton = document.querySelector<HTMLDivElement>('#inspect')
+  if (inspectButton) {
+    // eslint-disable-next-line no-console
+    inspectButton.onclick = () => console.log(globalThis.__inspect__())
   }
 }
 
