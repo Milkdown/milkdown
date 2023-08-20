2301207c4687f61dc6b771a981e1fb830fbbbb87
Mirone <Saul-Mirone@outlook.com>
2023-08-20 15:45:08 +0800

    feat: 🎸 add support for escape * and _
    ✅ Closes: #1044

--- packages/preset-commonmark/src/plugin/inline-sync-plugin/regexp.ts
+++ packages/preset-commonmark/src/plugin/inline-sync-plugin/regexp.ts
@@ -6,3 +6,10 @@ export const keepLinkRegexp = /\[(?<span>((www|https:\/\/|http:\/\/)[^\s\]]+))]\
 
 export const punctuationRegexp = (holePlaceholder: string) =>
   new RegExp(`\\\\(?=[^\\w\\s${holePlaceholder}\\\\]|_)`, 'g')
+
+export const ZERO_WIDTH_SPACE = '\u200B'
+
+export const asterisk = `${ZERO_WIDTH_SPACE}*${ZERO_WIDTH_SPACE}`
+export const asteriskHolder = `${ZERO_WIDTH_SPACE}＊${ZERO_WIDTH_SPACE}`
+export const underline = `${ZERO_WIDTH_SPACE}_${ZERO_WIDTH_SPACE}`
+export const underlineHolder = `${ZERO_WIDTH_SPACE}⎽${ZERO_WIDTH_SPACE}`
