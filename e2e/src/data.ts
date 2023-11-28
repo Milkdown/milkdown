67517e7b212a661db440868519cf4d59488fd736
Mirone <Saul-Mirone@outlook.com>
2023-11-28 10:35:56 +0800

    chore: optimize e2e home page (#1176)

---
+++ e2e/src/data.ts
@@ -0,0 +1,55 @@
+/* Copyright 2021, Milkdown by Mirone. */
+import type { CardComponentProps } from './main'
+
+export const cards: CardComponentProps[] = [
+  {
+    title: 'Commonmark',
+    description: 'Commonmark spec preset',
+    link: '/preset-commonmark/',
+  },
+  {
+    title: 'GFM',
+    description: 'GFM spec preset',
+    link: '/preset-gfm/',
+  },
+  {
+    title: 'Listener',
+    description: 'Listener plugin',
+    link: '/plugin-listener/',
+  },
+  {
+    title: 'Clipboard',
+    description: 'Clipboard plugin',
+    link: '/plugin-clipboard/',
+  },
+  {
+    title: 'Automd',
+    description: 'Automd plugin with GFM and listener',
+    link: '/plugin-automd/',
+  },
+  {
+    title: 'Math',
+    description: 'Math plugin',
+    link: '/plugin-math/',
+  },
+  {
+    title: 'Multi Editor',
+    description: 'Multi editor instances',
+    link: '/multi-editor/',
+  },
+  {
+    title: 'Image Block',
+    description: 'Image block component provides size adjustment',
+    link: '/image-block/',
+  },
+  {
+    title: 'Code Block',
+    description: 'Codemirror block component',
+    link: '/code-block/',
+  },
+  {
+    title: 'List Item Block',
+    description: 'List item block component',
+    link: '/list-item-block/',
+  },
+]
