5d34fd584532cb3aea747ca20b3e97af277422f9
Mirone <Saul-Mirone@outlook.com>
2023-12-11 14:00:04 +0800

    fix: 🐛 missing twemoji type (#1190)
    ✅ Closes: #1177

--- packages/plugin-emoji/src/__internal__/parse.ts
+++ packages/plugin-emoji/src/__internal__/parse.ts
@@ -3,6 +3,8 @@ import twemoji from 'twemoji'
 
 const setAttr = (text: string) => ({ title: text })
 
+export type TwemojiOptions = Parameters<typeof twemoji.parse>[1]
+
 export function parse(emoji: string, twemojiOptions?: TwemojiOptions): string {
   return twemoji.parse(emoji, { attributes: setAttr, base: 'https://cdn.jsdelivr.net/gh/twitter/twemoji/assets/', ...twemojiOptions }) as unknown as string
 }
