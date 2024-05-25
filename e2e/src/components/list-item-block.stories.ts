c3ed2f7f194311bb230ee18a9584d506e1d2bf6d
Mirone <Saul-Mirone@outlook.com>
2024-05-25 13:12:16 +0900

    test: add list item block in stories (#1338)

---
+++ e2e/src/components/list-item-block.stories.ts
@@ -0,0 +1,88 @@
+import type { Meta, StoryObj } from '@storybook/html'
+import { Editor, defaultValueCtx, editorViewOptionsCtx, rootCtx } from '@milkdown/core'
+import { nord } from '@milkdown/theme-nord'
+import { gfm } from '@milkdown/preset-gfm'
+import { commonmark } from '@milkdown/preset-commonmark'
+import { history } from '@milkdown/plugin-history'
+import { listItemBlockComponent } from '@milkdown/components/list-item-block'
+
+import '@milkdown/theme-nord/style.css'
+import '../style.css'
+import './list-item-block.css'
+
+const meta: Meta = {
+  title: 'Components/List Item Block',
+}
+
+export default meta
+
+interface Args {
+  readonly: boolean
+  defaultValue: string
+}
+
+const bullet = `
+- List item 1
+    - List item 1.1
+    - List item 1.2
+- List item 2
+- List item 3
+`
+
+const ordered = `
+1. List item 1
+    1. List item 1.1
+    2. List item 1.2
+2. List item 2
+3. List item 3
+`
+
+const todo = `
+- [ ] Todo list item 1
+    - [ ] Todo List item 1.1
+    - [ ] Todo List item 1.2
+- [ ] Todo list item 2
+- [ ] Todo list item 3
+`
+
+export const Bullet: StoryObj<Args> = {
+  render: (args) => {
+    const root = document.createElement('div')
+    Editor.make()
+      .config((ctx) => {
+        ctx.set(rootCtx, root)
+        ctx.set(defaultValueCtx, args.defaultValue)
+        ctx.set(editorViewOptionsCtx, {
+          editable: () => !args.readonly,
+        })
+      })
+      .config(nord)
+      .use(commonmark)
+      .use(gfm)
+      .use(listItemBlockComponent)
+      .use(history)
+      .create()
+
+    return root
+  },
+  args: {
+    readonly: false,
+    defaultValue: bullet,
+  },
+}
+
+export const Ordered: StoryObj<Args> = {
+  ...Bullet,
+  args: {
+    readonly: false,
+    defaultValue: ordered,
+  },
+}
+
+export const Todo: StoryObj<Args> = {
+  ...Bullet,
+  args: {
+    readonly: false,
+    defaultValue: todo,
+  },
+}
