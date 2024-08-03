f232ca7b02f05063ea11723eaab56a83fcdfc8f8
Mirone <Saul-Mirone@outlook.com>
2024-08-03 15:36:41 +0800

    ci: bump to latest nx cloud (#1454)
    * ci: bump to latest nx cloud
    
    * chore: fix
    
    * ci: improve
    
    * chore: remove unneeded
    
    * chore: retry
    
    * chore: update secrets
    
    * chore: fix
    
    * chore: fix e2e
    
    * chore: fix
    
    * chore: fix
    
    * chore: fix
    
    * chore: fix
    
    * chore: remove unused

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -1,7 +1,21 @@
+import * as process from 'node:process'
 import { defineConfig } from 'cypress'
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset'
 
 export default defineConfig({
-  projectId: 'mhq9z6',
-  e2e: {},
-  retries: 3,
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'cypress',
+      bundler: 'vite',
+      webServerConfig: {
+        reuseExistingServer: true,
+      },
+      webServerCommands: {
+        default: 'nx run @milkdown/e2e:start',
+      },
+      ciWebServerCommand: 'nx run @milkdown/e2e:start',
+    }),
+    baseUrl: 'http://localhost:5173',
+  },
+  retries: process.env.CI ? 2 : 0,
 })
--- e2e/vite.config.mts
+++ e2e/vite.config.mts
@@ -1,26 +1,10 @@
-import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
-import { cases } from './src/data'
-
-const data = cases.reduce((acc, { link }) => {
-  const name = link.match(/[\w-]+/)?.[0] as string
-  return {
-    ...acc,
-    [name]: resolve(__dirname, `src${link}index.html`),
-  }
-}, {} as Record<string, string>)
 
 export default defineConfig({
   root: 'src',
   build: {
     outDir: '../lib',
     emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/index.html'),
-        ...data,
-      },
-    },
   },
   server: {
     fs: {
