457f462ed527797a1daccc7e608e32b7437e04ef
Mirone <Saul-Mirone@outlook.com>
2024-08-08 12:14:27 +0800

    chore: fix

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -1,7 +1,6 @@
 import * as process from 'node:process'
 import { defineConfig } from 'cypress'
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset'
-import { input } from './entry'
 
 export default defineConfig({
   e2e: {
@@ -15,13 +14,6 @@ export default defineConfig({
         default: 'nx run @milkdown/e2e:start',
       },
       ciWebServerCommand: 'nx serve @milkdown/e2e',
-      viteConfigOverrides: {
-        build: {
-          rollupOptions: {
-            input,
-          },
-        },
-      },
     }),
     baseUrl: `http://localhost:${process.env.CI ? 4173 : 5173}`,
   },
