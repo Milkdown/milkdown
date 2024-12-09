a1f3676217372e373d6824d531ef5ac41efbe483
Mirone <Saul-Mirone@outlook.com>
2024-12-09 23:29:45 +0800

    feat: 🎸 prevent duplicated ids for headings (#1580)
    * feat: 🎸 prevent duplicated ids for headings
    
    ✅ Closes: #1567
    
    * test: add e2e

--- e2e/tests/input/heading.spec.ts
+++ e2e/tests/input/heading.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from '@playwright/test'
-import { focusEditor, getMarkdown } from '../misc'
+import { focusEditor, getMarkdown, waitNextFrame } from '../misc'
 
 test.beforeEach(async ({ page }) => {
   await page.goto('/preset-commonmark/')
@@ -20,4 +20,20 @@ test('heading', async ({ page }) => {
   await expect(editor.locator('h2')).toHaveAttribute('id', 'heading-2')
   const markdown2 = await getMarkdown(page)
   expect(markdown2).toBe('# Heading1\n\n## Heading 2\n')
+})
+
+test('should generate different id for same heading', async ({ page }) => {
+  const editor = page.locator('.editor')
+  await focusEditor(page)
+  await page.keyboard.type('# Heading1')
+  await page.keyboard.press('Enter')
+  await page.keyboard.type('# Heading1')
+  await page.keyboard.press('Enter')
+  await page.keyboard.type('## Heading1')
+  await page.keyboard.press('Enter')
+
+  await waitNextFrame(page)
+  await expect(editor.locator('h1').first()).toHaveAttribute('id', 'heading1')
+  await expect(editor.locator('h1').last()).toHaveAttribute('id', 'heading1-#2')
+  await expect(editor.locator('h2')).toHaveAttribute('id', 'heading1-#3')
 })
