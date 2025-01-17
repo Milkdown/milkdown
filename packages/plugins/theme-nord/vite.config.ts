a5691e6740d4641011b1b997156e1fd3c6e318b9
Mirone <Saul-Mirone@outlook.com>
2025-01-17 17:14:20 +0800

    fix: 🐛 theme nord css generated file (#1629)
    ✅ Closes: #1596

--- packages/plugins/theme-nord/vite.config.ts
+++ packages/plugins/theme-nord/vite.config.ts
@@ -1,3 +1,8 @@
+import type { LibraryOptions } from 'vite'
 import { pluginViteConfig } from '../../../vite.config.mjs'
 
-export default pluginViteConfig(import.meta.url)
+export default pluginViteConfig(import.meta.url, {
+  build: {
+    lib: { cssFileName: 'style' } as LibraryOptions,
+  },
+})
