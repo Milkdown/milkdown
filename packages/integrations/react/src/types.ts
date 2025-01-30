5033486b50a745ccdc7e8c0be4555fdcb055eb38
Mirone <Saul-Mirone@outlook.com>
2025-01-30 16:41:15 +0900

    feat: make integrations accept crepe editor (#1649)

--- packages/integrations/react/src/types.ts
+++ packages/integrations/react/src/types.ts
@@ -1,8 +1,8 @@
+import type { Crepe } from '@milkdown/crepe'
 import type { Editor } from '@milkdown/kit/core'
 import type { Dispatch, RefObject, SetStateAction } from 'react'
 
-export type GetEditor = (container: HTMLElement) => Editor | undefined
-
+export type GetEditor = (container: HTMLElement) => Editor | Crepe | undefined
 export interface UseEditorReturn {
   readonly loading: boolean
   readonly get: () => Editor | undefined
--- packages/integrations/vue/src/types.ts
+++ packages/integrations/vue/src/types.ts
@@ -1,7 +1,8 @@
+import type { Crepe } from '@milkdown/crepe'
 import type { Editor } from '@milkdown/kit/core'
 import type { Ref } from 'vue'
 
-export type GetEditor = (container: HTMLDivElement) => Editor
+export type GetEditor = (container: HTMLDivElement) => Editor | Crepe
 
 export interface EditorInfoCtx {
   dom: Ref<HTMLDivElement | null>
