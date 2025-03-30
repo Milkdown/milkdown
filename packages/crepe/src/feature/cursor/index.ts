dae2031b9880f4864ab6e393ec971938f68b959e
Saul-Mirone <Saul-Mirone@outlook.com>
2025-03-30 16:26:20 +0800

    feat: 🎸 add bike-style virtual cursor

--- packages/crepe/src/feature/cursor/index.ts
+++ packages/crepe/src/feature/cursor/index.ts
@@ -1,10 +1,13 @@
 import { cursor, dropCursorConfig } from '@milkdown/kit/plugin/cursor'
+import { createVirtualCursor } from 'prosemirror-virtual-cursor';
 
 import type { DefineFeature } from '../shared'
+import { $prose } from '@milkdown/kit/utils';
 
 interface CursorConfig {
   color: string | false
   width: number
+  virtual: boolean
 }
 export type CursorFeatureConfig = Partial<CursorConfig>
 
@@ -21,4 +24,11 @@ export const defineFeature: DefineFeature<CursorFeatureConfig> = (
       }))
     })
     .use(cursor)
+
+  if (config?.virtual === false) {
+    return;
+  }
+
+  const virtualCursor = createVirtualCursor()
+  editor.use($prose(() => virtualCursor))
 }
