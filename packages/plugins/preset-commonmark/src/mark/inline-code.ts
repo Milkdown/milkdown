ad1f55eec47e267a65334f81c2ffc03f1b69e69d
Saul-Mirone <Saul-Mirone@outlook.com>
2025-03-30 16:40:34 +0800

    fix: remove inline code mark inclusive config

--- packages/plugins/preset-commonmark/src/mark/inline-code.ts
+++ packages/plugins/preset-commonmark/src/mark/inline-code.ts
@@ -22,7 +22,6 @@ withMeta(inlineCodeAttr, {
 export const inlineCodeSchema = $markSchema('inlineCode', (ctx) => ({
   priority: 100,
   code: true,
-  inclusive: false,
   parseDOM: [{ tag: 'code' }],
   toDOM: (mark) => ['code', ctx.get(inlineCodeAttr.key)(mark)],
   parseMarkdown: {
