1d8f644b082ccbe94aa7b255422251294386f6aa
Saul-Mirone <Saul-Mirone@outlook.com>
2025-05-04 17:40:30 +0800

    test: add e2e

--- e2e/tests/transform/preserve-empty-line.spec.ts
+++ e2e/tests/transform/preserve-empty-line.spec.ts
@@ -11,12 +11,13 @@ test('preserve empty line', async ({ page }) => {
   const markdown = await loadFixture('preserve-empty-line.md')
   await setMarkdown(page, markdown)
 
-  expect(await page.locator('p').count()).toBe(5)
+  expect(await page.locator('p').count()).toBe(6)
   expect(await page.locator('p').nth(0).textContent()).toBe('123')
   expect(await page.locator('p').nth(1).textContent()).toBe('')
   expect(await page.locator('p').nth(2).textContent()).toBe('456')
   expect(await page.locator('p').nth(3).textContent()).toBe('')
   expect(await page.locator('li').nth(0).textContent()).toBe('')
+  expect(await page.locator('li').nth(1).textContent()).toBe('')
   expect(await page.locator('blockquote').nth(0).textContent()).toBe('')
 
   const markdownOutput = await getMarkdown(page)
