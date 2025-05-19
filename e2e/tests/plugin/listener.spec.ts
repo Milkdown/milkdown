dad228f44d520b7a1b306e7c23225d82135848c6
Mirone <Saul-Mirone@outlook.com>
2025-05-19 19:41:04 +0800

    feat: add selection change event in listern plugin (#1900)

--- e2e/tests/plugin/listener.spec.ts
+++ e2e/tests/plugin/listener.spec.ts
@@ -2,11 +2,8 @@ import { expect, test } from '@playwright/test'
 
 import { focusEditor, selectAll } from '../misc'
 
-test.beforeEach(async ({ page }) => {
-  await page.goto('/plugin-listener/')
-})
-
 test('on markdown updated', async ({ page }) => {
+  await page.goto('/plugin-listener/?type=markdown')
   await focusEditor(page)
   await selectAll(page)
   await page.keyboard.press('ArrowRight')
@@ -14,10 +11,27 @@ test('on markdown updated', async ({ page }) => {
   await page.keyboard.type('A')
   const msg = await msgPromise
   const [after, before] = msg.args()
-  expect(await after?.jsonValue()).toBe('testA\n')
-  expect(await before?.jsonValue()).toBe('test\n')
+  const afterText = await after?.jsonValue()
+  const beforeText = await before?.jsonValue()
+  expect(afterText).toBe('testA\n')
+  expect(beforeText).toBe('test\n')
 
   msgPromise = page.waitForEvent('console')
   await page.keyboard.type('A')
   await expect(msgPromise).resolves.toBeTruthy()
+})
+
+test('on selection updated', async ({ page }) => {
+  await page.goto('/plugin-listener/?type=selection')
+  await focusEditor(page)
+  await selectAll(page)
+  const msgPromise = page.waitForEvent('console')
+  await page.keyboard.press('ArrowRight')
+  const msg = await msgPromise
+  const [after, before] = msg.args()
+  const afterText = await after?.jsonValue()
+  const beforeText = await before?.jsonValue()
+
+  expect(afterText).toBe('5-5')
+  expect(beforeText).toBe('0-6')
 })
