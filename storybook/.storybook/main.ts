35272dbb38274fbb752d328b49c9a15b2bfbde45
Saul-Mirone <Saul-Mirone@outlook.com>
2025-05-29 09:39:04 +0800

    chore: bump storybook

--- storybook/.storybook/main.ts
+++ storybook/.storybook/main.ts
@@ -2,7 +2,6 @@ import type { StorybookConfig } from '@storybook/html-vite'
 
 const config: StorybookConfig = {
   stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-essentials'],
   staticDirs: ['../public'],
   framework: {
     name: '@storybook/html-vite',
