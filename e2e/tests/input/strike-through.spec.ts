fe34950ca9cf66bedef321d70a1210c5cacb68ab
David Tobin <dtobin@tines.io>
2025-07-17 12:10:32 +0100

    fix: strikethrough matching on intra-word tilde

--- e2e/tests/input/strike-through.spec.ts
+++ e2e/tests/input/strike-through.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test'
 
-import { focusEditor } from '../misc'
+import { focusEditor, getMarkdown } from '../misc'
 
 test.beforeEach(async ({ page }) => {
   await page.goto('/preset-gfm/')
@@ -11,4 +11,15 @@ test('strike through', async ({ page }) => {
   await focusEditor(page)
   await page.keyboard.type('The lunatic is ~~on the grass~~')
   await expect(editor.locator('del')).toHaveText('on the grass')
+})
+
+test('intra-word strike through', async ({ page }) => {
+  const editor = page.locator('.editor')
+  await focusEditor(page)
+  await page.keyboard.type('C:/the/~lunatic~/is/on/the/grass')
+  await expect(editor.locator('del')).toHaveCount(0)
+
+  const markdown = await getMarkdown(page)
+
+  expect(markdown).toBe('C:/the/\\~lunatic\\~/is/on/the/grass\n')
 })
