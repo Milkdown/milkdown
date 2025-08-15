d1a091ef0fa09e734c48e3eac0a17db4b393346b
Saul-Mirone <Saul-Mirone@outlook.com>
2025-08-16 00:49:13 +0900

    feat: add new highlight plugin

---
+++ packages/plugins/plugin-highlight/src/index.ts
@@ -0,0 +1,30 @@
+import { $ctx, $prose } from '@milkdown/utils'
+import { createHighlightPlugin } from 'prosemirror-highlight'
+
+type HighlightPluginOptions = Parameters<typeof createHighlightPlugin>[0]
+
+export const highlightPluginConfig = $ctx<
+  HighlightPluginOptions,
+  'highlightPluginConfig'
+>({} as HighlightPluginOptions, 'highlightPluginConfig')
+
+highlightPluginConfig.meta = {
+  package: '@milkdown/plugin-highlight',
+  displayName: 'Ctx<highlightPluginConfig>',
+}
+
+/// Config for the highlight plugin.
+export const highlightPlugin = $prose((ctx) => {
+  const config = ctx.get(highlightPluginConfig.key)
+  if (!config.parser) {
+    throw new Error(
+      'Highlight plugin requires a parser to be set in the highlightPluginConfig.'
+    )
+  }
+  return createHighlightPlugin(config)
+})
+
+highlightPlugin.meta = {
+  package: '@milkdown/plugin-highlight',
+  displayName: 'Highlight Plugin',
+}
