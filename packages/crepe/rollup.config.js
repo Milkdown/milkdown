50bd23baed6213d99ac54c0f6969a8cf24420439
Saul-Mirone <Saul-Mirone@outlook.com>
2025-08-25 15:13:51 +0900

    chore: f

--- packages/crepe/rollup.config.js
+++ packages/crepe/rollup.config.js
@@ -2,7 +2,6 @@ import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 import resolve from '@rollup/plugin-node-resolve'
 import esbuild from 'rollup-plugin-esbuild'
-import replace from '@rollup/plugin-replace'
 import pkg from './package.json' with { type: 'json' }
 
 const external = [
@@ -32,11 +31,6 @@ export default () => {
     json(),
     commonjs(),
     esbuild({ target: 'es2018' }),
-    replace({
-      __VUE_OPTIONS_API__: 'true',
-      __VUE_PROD_DEVTOOLS__: 'false',
-      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
-    }),
   ]
   return [
     ...entry.flatMap((name) => {
