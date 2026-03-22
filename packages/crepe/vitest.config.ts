93edf5baabd6f08699158f18a6a904f65172de0f
Saul-Mirone <Saul-Mirone@outlook.com>
2026-03-22 16:39:16 +0900

    fix: unit test

--- packages/crepe/vitest.config.ts
+++ packages/crepe/vitest.config.ts
@@ -1,6 +1,8 @@
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  plugins: [vueJsx()],
   test: {
     globals: true,
     environment: 'jsdom',
