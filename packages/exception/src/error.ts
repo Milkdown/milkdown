cbd88c5cb9a9f94afc6e4738832c55a2c4a525b9
Saul-Mirone <Saul-Mirone@outlook.com>
2026-04-19 18:54:09 +0900

    fix: explicitly assign cause for ES2018 compatibility

--- packages/exception/src/error.ts
+++ packages/exception/src/error.ts
@@ -2,9 +2,13 @@ import type { ErrorCode } from './code'
 
 export class MilkdownError extends Error {
   public code: string
+  public override cause?: unknown
   constructor(code: ErrorCode, message: string, options?: { cause?: unknown }) {
     super(message, options)
     this.name = 'MilkdownError'
     this.code = code
+    if (options?.cause !== undefined) {
+      this.cause = options.cause
+    }
   }
 }
